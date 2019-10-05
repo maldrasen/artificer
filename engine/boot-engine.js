cdae2545617ebd82c0b13c8502e848d25333d4a3
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-10-05 14:35:26 -0400

    Move stuff around a bit

--- engine/boot-engine.js
+++ engine/boot-engine.js
@@ -19,30 +19,33 @@ require(`${ROOT}/engine/forms/mission`);
 require(`${ROOT}/engine/forms/project`);
 require(`${ROOT}/engine/forms/species`);
 
+// === Helpers ===
+require(`${ROOT}/engine/helpers/character-agent`);
+
 // === Scrutinizers ===
 require(`${ROOT}/engine/scrutinizers/central-scrutinizer`);
 require(`${ROOT}/engine/scrutinizers/synchronized-scrutinizer`);
 
 // === Services ===
 require(`${ROOT}/engine/services/composer`);
-require(`${ROOT}/engine/services/character-agent`);
 
-// === Builders ===
-require(`${ROOT}/engine/services/character-builder/adjustments`);
-require(`${ROOT}/engine/services/character-builder/anus-builder`);
-require(`${ROOT}/engine/services/character-builder/body-builder`);
-require(`${ROOT}/engine/services/character-builder/character-builder`);
-require(`${ROOT}/engine/services/character-builder/cock-builder`);
-require(`${ROOT}/engine/services/character-builder/mouth-builder`);
-require(`${ROOT}/engine/services/character-builder/name-builder`);
-require(`${ROOT}/engine/services/character-builder/nipples-builder`);
-require(`${ROOT}/engine/services/character-builder/pussy-builder`);
-require(`${ROOT}/engine/services/character-builder/tits-builder`);
-require(`${ROOT}/engine/services/character-builder/name-generators/demon-name-generator`);
-require(`${ROOT}/engine/services/character-builder/name-generators/elf-name-generator`);
-require(`${ROOT}/engine/services/character-builder/name-generators/goblin-name-generator`);
-require(`${ROOT}/engine/services/character-builder/name-generators/kobold-name-generator`);
-require(`${ROOT}/engine/services/character-builder/name-generators/rat-name-generator`);
+// === Factories ===
+require(`${ROOT}/engine/factories/character-builder/adjustments`);
+require(`${ROOT}/engine/factories/character-builder/anus-builder`);
+require(`${ROOT}/engine/factories/character-builder/body-builder`);
+require(`${ROOT}/engine/factories/character-builder/character-builder`);
+require(`${ROOT}/engine/factories/character-builder/cock-builder`);
+require(`${ROOT}/engine/factories/character-builder/mouth-builder`);
+require(`${ROOT}/engine/factories/character-builder/name-builder`);
+require(`${ROOT}/engine/factories/character-builder/nipples-builder`);
+require(`${ROOT}/engine/factories/character-builder/pussy-builder`);
+require(`${ROOT}/engine/factories/character-builder/tits-builder`);
+
+require(`${ROOT}/engine/factories/name-generators/demon-name-generator`);
+require(`${ROOT}/engine/factories/name-generators/elf-name-generator`);
+require(`${ROOT}/engine/factories/name-generators/goblin-name-generator`);
+require(`${ROOT}/engine/factories/name-generators/kobold-name-generator`);
+require(`${ROOT}/engine/factories/name-generators/rat-name-generator`);
 
 // === Utilities ===
 require(`${ROOT}/engine/utilities/array-utility.js`);
