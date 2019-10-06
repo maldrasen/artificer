16a4d0e243bad9a3b42c7753a8b65eec34e21b60
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-10-06 17:19:14 -0400

    Update events

--- data/artificer/events/project/clear-great-hall-sleep-no.js
+++ data/artificer/events/project/clear-great-hall-sleep-no.js
@@ -1,12 +1,13 @@
 Event.build('clear-great-hall-sleep-no', {
   background: '../../resources/backgrounds/bg-courtyard-ruined-night.png',
-
   stages:[
-    {
-      pages: [
-        { text:'Nope' }
-      ]
-    }
+    { pages: [
+      { text:`No.` },
+      { text:`I think not.` },
+      { text:`Allowing that kind of affection is I think unbecoming of the {{P::character.title}} of this keep.` },
+      { text:`I shoo them away from my fire and the immediate vicinity.` },
+      { text:`As it is, I'm sure they're infested with fleas who know what diseases.` },
+      { text:`They'll have to find somewhere else for their ratty little orgy.` },
+    ]}
   ]
-
 });
--- data/artificer/events/project/clear-great-hall-sleep-yes.js
+++ data/artificer/events/project/clear-great-hall-sleep-yes.js
@@ -1,12 +1,13 @@
 Event.build('clear-great-hall-sleep-yes', {
   background: '../../resources/backgrounds/bg-courtyard-ruined-night.png',
-
   stages:[
-    {
-      pages: [
-        { text:'just sleep' }
-      ]
-    }
+    { pages: [
+      { text:`Well, I don't see any reason why they shouldn't be allowed to join me.` },
+      { text:`I give the group a slow nod and they begin to surround me, cuddling up to me until I'm packed in and surrounded by their furry little bodies.` },
+      { text:`I make it clear to them though that I'm only interested in sleeping tonight.` },
+      { text:`I yawn and lay my head across a rat near the top of the pile, using her soft body as a pillow.` },
+      { text:`Though there's some squirming and some slow lazy fondling among the rats in the pile, for the most part they behave themselves.` },
+      { text:`And I'm finally able to get a decent night's rest...` },
+    ]}
   ]
-
 });
