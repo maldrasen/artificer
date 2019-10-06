00f5d2bf7f099b46f3f191d682604b58df3c1574
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-10-06 08:50:09 -0400

    Add event

---
+++ data/artificer/events/project/clear-great-hall-start.js
@@ -0,0 +1,13 @@
+Event.build('clear-great-hall-start', {
+  background: '../../resources/backgrounds/bg-courtyard-ruined-night.png',
+
+  stages:[
+    {
+      pages: [
+        { text:"Warm Rats Event" },
+        { text:`(I can now use the map to walk around the keep.)`, notification:"<span class='boom'>Unlocked:</span> The Map" }
+      ]
+    }
+  ],
+
+});
