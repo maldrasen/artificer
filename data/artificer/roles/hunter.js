f8d9dcb27e6248fb3314cd27042aa4447b60b9e1
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-10-07 19:21:16 -0400

    Add roles

---
+++ data/artificer/roles/hunter.js
@@ -0,0 +1,4 @@
+Role.build('hunder', {
+  name: 'Hunter',
+  description: 'A hunter will hunt animals for meat and hides. They can also capture animals.',
+});
---
+++ data/artificer/roles/rest.js
@@ -0,0 +1,4 @@
+Role.build('rest', {
+  name: 'Rest',
+  description: 'Is minion will rest and relax. Resing minions heal faster and will entertain themselves.',
+});
