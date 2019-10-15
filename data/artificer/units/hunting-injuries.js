a235eefc54fe7b0953d7d6d4a237fd09d1d90592
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-10-14 21:24:42 -0400

    Yep, this is going to take forever.

--- data/artificer/units/hunting-injuries.js
+++ data/artificer/units/hunting-injuries.js
@@ -1,25 +1,47 @@
 
 // === Painful Injuries ===
 
-Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'rip', level:3,
-  requires: ['failure'],
-  story: `As {{H::character.firstName}} was stalking {{H::gender.his}} prey {{H::gender.he}} was attacked by a
-          ferocious Dick Wolf. The beast spent most of the afternoon anally raping {{H::gender.him}} until
-          {{H::gender.he}} was finally able to escape.`
-});
+// --- Anus : Cut ---
 
-Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'pierce', level:1,
-  requires: ['tier-0'],
-  story: `While {{H::character.firstName}} was hiding in a bush, a pixie snuck up behind {{H::gender.him}} and jabbed a
-          sharpened stick right up {{H::gender.his}} exposed ass and flew away laughing.`,
+Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'cut', level:2,
+  requires: ['tier-0','success'],
+  story: `{{H::character.firstName}} has already finished hunting and was on {{H::gender.his}} way back to the keep
+          when {{H::gender.he}} decided to take a short break. {{H::gender.He}} plopped down onto a comfortable looking
+          log but carelessly sat down on the sharp rock {{H::gender.he}}'d been using as a hunting knife. The makeshift
+          knife opened a nasty, ragged looking gash right along {{H::gender.his}} anus.`
 });
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'cut', level:5,
   requires: ['tier-0','failure'],
   story: `{{H::character.firstName}} was climbing up a tree, chasing a squirrel, when the branch {{H::gender.he}} was
           on snapped. {{H::gender.He}} fell directly onto a small sapling and somehow managed to get the entire young
-          tree lodged in {{H::gender.his}} ass. The tree's thorney branches severly scraped and tore {{H::gender.his}}
-          colon and asshole as {{H::gender.he}} pulled himself off of the tree impaling {{H::gender.him}}.`,
+          tree lodged in {{H::gender.his}} ass. The tree's thorny branches severely scraped and tore {{H::gender.his}}
+          colon and asshole as {{H::gender.he}} pulled himself off of the tree impaling {{H::gender.him}}.`
+});
+
+// --- Anus : Pierce ---
+
+Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'pierce', level:1,
+  requires: ['tier-0'],
+  story: `While {{H::character.firstName}} was hiding in a bush, a pixie snuck  up behind {{H::gender.him}} and jabbed a
+          sharpened stick right up {{H::gender.his}} exposed ass and flew away laughing.`
+});
+
+// --- Anus : Rip ---
+
+Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'rip', level:2,
+  requires: ['failure','species-rat'],
+  story: `{{H::character.firstName}} was found and captured by a group of Kobolds who were also out hunting. They tied
+          {{H::gender.him}} to a tree and made a game out of guessing how many pine cones they could fit into
+          {{H::gender.his}} ass at once. (The answer was {{randomEng|3-6}}.) The ordeal though left {{H::gender.him}}
+          with a stretched and bleeding asshole.`
+});
+
+Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'rip', level:3,
+  requires: ['failure'],
+  story: `As {{H::character.firstName}} was stalking {{H::gender.his}} prey {{H::gender.he}} was attacked by a
+          ferocious Dick Wolf. The beast spent most of the afternoon anally raping {{H::gender.him}} until
+          {{H::gender.he}} was finally able to escape.`
 });
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'rip', level:4,
@@ -28,43 +50,61 @@ Role.Hunter.Injuries.addPossibleInjury({ location:'anus', type:'rip', level:4,
           {{H::gender.his}} head stuck in the tight hole. {{H::gender.He}} struggled there for quite a while with
           {{H::gender.his}} wiggling ass lifted high in the air. It was late in the evening by the time
           {{H::gender.he}} managed to free himself, but only almost every every animal within a mile radius had a
-          chance to rape {{H::gender.his}} exposed asshole. Even the rabbit {{H::gender.he}} was chasing took a turn.`,
+          chance to rape {{H::gender.his}} exposed asshole. Even the rabbit {{H::gender.he}} was chasing took a turn.`
 });
+
+// --- Nipples : Blight ---
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'nipples', type:'blight', level:2,
   requires: ['failure'],
   story: `While {{H::character.firstName}} was stalking {{H::gender.his}} prey through some deep brush {{H::gender.he}}
           felt a sudden stabbing pain in his chest. A spider had attached itself to one of {{H::gender.his}} nipples
           and was pumping it full of venom. {{H::gender.He}} managed to brush it off, but not until {{H::gender.his}}
-          nipple had swollen up to twice its normal size.`,
+          nipple had swollen up to twice its normal size.`
 });
+
+// --- Cock : Blight ---
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'cock', type:'blight', level:5,
   requires: ['failure','tier-0','species-rat'],
   story: `{{H::character.firstName}} was chasing a fox through the nearby woods when {{H::gender.he}} ran face first
-          into a massive spiderweb, becomming immeadietly entangled. One of the local dryads, tired of your rats
+          into a massive spiderweb, becoming immediately entangled. One of the local dryads, tired of your rats
           hunting her animal friends, set the trap in order to spend the afternoon teaching {{H::gender.him}} a lesson
           by tormenting {{H::gender.his}} cock. {{H::gender.He}} was finally able to limp home that evening, but only
           after having spent the last several hours getting {{H::gender.his}} small dick stung and chewed on by
-          centipedes, spiders, and wasps.`,
+          centipedes, spiders, and wasps.`
 });
+
+// --- Pussy : Pierce ---
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'pussy', type:'pierce', level:3,
   requires: ['tier-0'],
   story: `While {{H::character.firstName}} was hiding in a bush, a pixie snuck up behind {{H::gender.him}} and jabbed a
-          sharpened stick completely through one of {{H::gender.his}} exposed labia and flew away laughing.`,
+          sharpened stick completely through one of {{H::gender.his}} exposed labia and flew away laughing.`
+});
+
+// --- Pussy : Rip ---
+
+Role.Hunter.Injuries.addPossibleInjury({ location:'pussy', type:'rip', level:2,
+  requires: ['failure','species-rat'],
+  story: `{{H::character.firstName}} was found and captured by a group of Kobolds who were also out hunting. They tied
+          {{H::gender.him}} to a tree and made a game out of guessing how many river stones they could fit into
+          {{H::gender.his}} pussy at once. (The answer was {{randomEng|8-16}}.) The ordeal though left {{H::gender.him}}
+          with a stretched and bleeding cunt.`
 });
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'pussy', type:'rip', level:4,
   story: `While out hunting, {{H::character.firstName}} was taken captive by a lupin hunting band. They agreed to let
           {{H::gender.him}} go only if {{H::gender.he}} would fuck them all. Their large knotted cocks left
-          {{H::gender.his}} pussy ripped asunder.`,
+          {{H::gender.his}} pussy ripped asunder.`
 });
+
+// --- Tits : Smash ---
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'tits', type:'smash', level:2,
   story: `{{H::character.firstName}} had a wild boar cornered and was going in for the kill when it suddenly charged.
           {{H::gender.He}} tride to dodge to the side, but the boar crashed into {{H::gender.his}} chest, crushing
-          {{H::gender.his}} tits with it's tusks.`,
+          {{H::gender.his}} tits with it's tusks.`
 });
 
 // === Critical Injuries ===
@@ -72,7 +112,7 @@ Role.Hunter.Injuries.addPossibleInjury({ location:'tits', type:'smash', level:2,
 Role.Hunter.Injuries.addPossibleInjury({ location:'body', type:'smash', level:1,
   story: `{{H::character.firstName}} had to flee from an Abomination as {{H::gender.he}} was returning home from
           {{H::gender.his}} hunt. Although he {{H::gender.he}} managed to get away, {{H::gender.he}} had to jump down
-          into a deep ravine and broke several ribs.`,
+          into a deep ravine and broke several ribs.`
 });
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'body', type:'blight', level:1,
@@ -83,5 +123,5 @@ Role.Hunter.Injuries.addPossibleInjury({ location:'body', type:'blight', level:1
 
 Role.Hunter.Injuries.addPossibleInjury({ location:'head', type:'smash', level:1,
   story: `{{H::character.firstName}} was stalking {{H::gender.his}} prey through a deep ravine, when a loose rock fell
-          from the cliffs above, striking {{H::gender.him}} in the head.`,
+          from the cliffs above, striking {{H::gender.him}} in the head.`
 });
