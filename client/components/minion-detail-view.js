4f4085ff41c192c05d54599901e023a415196d79
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-10-19 17:28:32 -0400

    More minion details

--- client/components/minion-detail-view.js
+++ client/components/minion-detail-view.js
@@ -10,8 +10,23 @@ Components.MinionDetailView = (function() {
   }
 
   function build() {
-    console.log("Build Minion View:",minion);
+    let view = $('#overlayContent .minion-detail-view');
+
+    view.find('.portrait-frame').append($('<img>',{ src:minion.portrait }));
+    view.find('.name-section .name').append(minion.name);
+    view.find('.name-section .gender').append(minion.gender);
+    view.find('.name-section .species').append(minion.species);
+    view.find('.top-row .health-value').append(minion.health);
+    view.find('.top-row .health-word').append(minion.healthWord);
+    view.find('.top-row .health-section').addClass(`fg-health-${minion.healthClass}`);
+    view.find('.attributes dd.physical').append(minion.physical);
+    view.find('.attributes dd.personal').append(minion.personal);
+    view.find('.attributes dd.mental').append(minion.mental);
+    view.find('.attributes dd.magical').append(minion.magical);
+
+    Elements.ScrollingPanel.build($('#overlayContent .scrolling-panel'));
   }
+
 
   return { init, open, build };
 
