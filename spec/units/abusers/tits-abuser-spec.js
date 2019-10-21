f240cae79fa592246f9831babb6da023f7513409
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-10-20 21:03:16 -0400

    Fix spec

--- spec/units/abusers/tits-abuser-spec.js
+++ spec/units/abusers/tits-abuser-spec.js
@@ -2,7 +2,7 @@ describe('Abuser: Tits', function() {
 
   it('Blight, level 1', function(done) {
     CharacterBuilder.build({ gender:'female', species:'equian' }).then(character => {
-      character.addInjury({ location:'tits', type:'blight', level:1 }).then(injury => {
+      character.addInjury({ location:'tits', type:'burn', level:1 }).then(injury => {
         done();
       });
     });
