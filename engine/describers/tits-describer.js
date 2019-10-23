f0b82c17eacb453eb74365bb96a9048ec3c443dd
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-10-22 22:51:14 -0400

    Rearranging things yet again

--- engine/describers/tits-describer.js
+++ engine/describers/tits-describer.js
@@ -57,7 +57,49 @@ global.TitsDescriber = class TitsDescriber {
   }
 
   d_rat_tits() {
-    return "[Rat Tits, again]"
+    if (this.tits.shape == 'flat' && this.tits.sizeClass == 'zero') { return Random.from([
+      `{{C::character.firstName}} has a completely flat chest. {{C::gender.His}} chest is lean and muscular and could easily be mistaken for a man's if not for {{C::gender.his}} many prominent nipples.`,
+      `{{C::character.firstName}} has a lean muscular chest that could easily be mistaken for a man's if not for {{C::gender.his}} many prominent nipples.`,
+      `{{C::character.firstName's}} chest is washboard flat, with absolutely no {{tits}} to speak of.`,
+    ]); }
+
+    if (this.tits.shape == 'conical' && this.tits.sizeClass == 'tiny') { return Random.from([
+      `{{C::character.firstName's}} chest is studded with twelve tiny conical {{tits}}. {{C::gender.his}} {{tits}} are too small to do much more than push {{C::gender.his}} many prominent nipples out just a bit further.`,
+      `{{C::character.firstName}} has twelve tiny cone shaped {{tits}}. They're each quite small and don't do much other than press {{C::gender.his}} many prominent nipples out just a bit further.`,
+      `{{C::character.firstName}} has six rows of tiny conical {{tits}}. Each of {{C::gender.his}} {{tits}} is capped by a prominent nipple almost as large as the {{tit}} itself.`,
+    ]); }
+
+    if (this.tits.shape == 'conical' && this.tits.sizeClass == 'small') { return Random.from([
+      `{{C::character.firstName's}} chest is studded with twelve small conical {{tits}}. {{C::gender.his}} {{tits}} are wide at the base and come to a narrow tip, which makes {{C::gender.his}} prominent nipples seem to poke out even further.`,
+      `{{C::character.firstName}} has twelve small cone shaped {{tits}}. Each of {{C::gender.his}} small {{tits}} are wide at the base and come to a narrow tip, then capped with a thick and prominent nipple.`,
+      `{{C::character.firstName}} has six rows of small conical {{tits}}. Each of {{C::gender.his}} {{tits}} is capped by a prominent nipple almost as large as the {{tit}} itself.`,
+    ]); }
+
+    if (this.tits.shape == 'perky' && this.tits.sizeClass == 'tiny') { return Random.from([
+      `{{C::character.firstName's}} {{tits}} are tiny and perky, with prominent upturned nipples set high on each of {{C::gender.his}} {{tits}}.`,
+      `{{C::character.firstName's}} chest is adorned with twelve tiny {{tits}}. Each of {{C::gender.his}} perky upturned {{tits}} is too small to do much more than push {{C::gender.his}} many prominent nipples out just a bit further.`,
+      `{{C::character.firstName}} has six rows of tiny {{tits}}. {{C::gender.His}} twelve prominent nipples are set high on each of {{C::gender.his}} perky tits, making them look even more jutting and pinchable.`
+    ]); }
+
+    if (this.tits.shape == 'perky' && this.tits.sizeClass == 'small') { return Random.from([
+      `{{C::character.firstName's}} {{tits}} are small and perky, with prominent upturned nipples set high on each of {{C::gender.his}} {{tits}}.`,
+      `{{C::character.firstName's}} chest is adorned with twelve small {{tits}}. Each of {{C::gender.his}} perky upturned {{tits}} pushes {{C::gender.his}} prominent nipples upward proudly.`,
+      `{{C::character.firstName}} has six rows of small {{tits}}. {{C::gender.His}} twelve prominent nipples are set high on each of {{C::gender.his}} perky tits, making them look even more jutting and pinchable.`
+    ]); }
+
+    if (this.tits.shape == 'round' && this.tits.sizeClass == 'tiny') { return Random.from([
+      `{{C::character.firstName's}} {{tits}} are round but tiny. Each of {{C::gender.his}} tiny round orbs is slightly smaller than a lemon and capped with a thick prominent nipple.`,
+      `{{C::character.firstName's}} chest is studded with twelve tiny {{tits}}. Each of {{C::gender.his}} round {{tits}} are slightly smaller than a lemon and capped with a thick prominent nipple.`,
+      `{{C::character.firstName}} has twelve round but tiny {{tits}}. Each of {{C::gender.his}} tits are slightly smaller than a lemon and capped with a thick prominent nipple.`,
+    ]); }
+
+    if (this.tits.shape == 'round' && this.tits.sizeClass == 'small') { return Random.from([
+      `{{C::character.firstName's}} {{tits}} are small and round. Each of {{C::gender.his}} round {{tits}} is slightly larger than a lemon and capped with a thick prominent nipple.`,
+      `{{C::character.firstName's}} chest is studded with twelve small {{tits}}. Each of {{C::gender.his}} round {{tits}} are slightly larger than a lemon and capped with a thick prominent nipple.`,
+      `{{C::character.firstName}} has twelve small round {{tits}}. Each of {{C::gender.his}} tits are slightly larger than a lemon and capped with a thick prominent nipple.`,
+    ]); }
+
+    return Weaver.error(`TODO: Describe rat tits of larger sizes. Shape:${this.tits.shape} Size:${this.tits.sizeClass}`)
   }
 
   d_normal() {
@@ -148,117 +190,6 @@ global.TitsDescriber = class TitsDescriber {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let key = `smash.${tits.smashLevel}`
-//
-// let description = Random.from(Description.where(desc => {
-//   if (tits.count > 2)      { return false; } // TODO: Handle smashed multi breasts.
-//   if (desc.type != 'tits') { return false; }
-//   if (desc.injury != key)  { return false; }
-//   if (desc.count == 'one'    && tits.smashCount != 1)     { return false; }
-//   if (desc.place == 'single' && tits.smashPlace == 'all') { return false; }
-//   if (desc.place == 'all'    && tits.smashPlace != 'all') { return false; }
-//   return true;
-// }));
-//
-// console.log("GOT Desc: ",description.properties);
-//
-// return description.template;
-
-
-// global.TitsDescriber = (function() {
-//
-//   // The updateDescription() function takes all of the attributes on the tits
-//   // and builds a description, which it then caches on the tits model itself.
-//
-//
-//
-//   function syncFullDescription(character, parts) {
-//     // const blighted = injuries.filter(injury => { return injury.location == 'tits' && injury.damageType == 'blight' })[0];
-//     // const wounds = injuryDescriptions(injuries);
-//
-//     // The blighted description supercedes the normal tits description, though
-//     // other injury descriptions are still tacked on at the end.
-//     // if (blighted) { return `${blighted.description} ${wounds}`; }
-//
-//     if (parts.tits == null) { return `${maleDescription(character, parts)}` }
-//     if (character.speciesCode == 'rat') { return `${ratTits(character, parts.tits)} ${describeNipples(character, parts.nipples)}` }
-//
-//     return Weaver.error('Needs more tits!')
-//   }
-//
-//   function maleDescription(character, parts) {
-//     return "[TODO: Male Nipples]"
-//   }
-//
-//   // Rats are unique among the characters because they have 12 tits, arranged
-//   // in two rows. They're usually flat chested, but can have prominent nipples.
-//   function ratTits(character, tits) {
-//     if (tits.shape == 'flat' && tits.sizeClass == 'zero') { return Random.from([
-//       `{{C::character.firstName}} has a completely flat chest. {{C::gender.His}} chest is lean and muscular and could easily be mistaken for a man's if not for {{C::gender.his}} many prominent nipples.`,
-//       `{{C::character.firstName}} has a lean muscular chest that could easily be mistaken for a man's if not for {{C::gender.his}} many prominent nipples.`,
-//       `{{C::character.firstName's}} chest is washboard flat, with absolutely no ${_tits()} to speak of.`,
-//     ]); }
-//
-//     if (tits.shape == 'conical' && tits.sizeClass == 'tiny') { return Random.from([
-//       `{{C::character.firstName's}} chest is studded with twelve tiny conical ${_tits()}. {{C::gender.his}} ${_tits()} are too small to do much more than push {{C::gender.his}} many prominent nipples out just a bit further.`,
-//       `{{C::character.firstName}} has twelve tiny cone shaped ${_tits()}. They're each quite small and don't do much other than press {{C::gender.his}} many prominent nipples out just a bit further.`,
-//       `{{C::character.firstName}} has six rows of tiny conical ${_tits()}. Each of {{C::gender.his}} ${_tits()} is capped by a prominent nipple almost as large as the ${_tit()} itself.`,
-//     ]); }
-//
-//     if (tits.shape == 'conical' && tits.sizeClass == 'small') { return Random.from([
-//       `{{C::character.firstName's}} chest is studded with twelve small conical ${_tits()}. {{C::gender.his}} ${_tits()} are wide at the base and come to a narrow tip, which makes {{C::gender.his}} prominent nipples seem to poke out even further.`,
-//       `{{C::character.firstName}} has twelve small cone shaped ${_tits()}. Each of {{C::gender.his}} small ${_tits()} are wide at the base and come to a narrow tip, then capped with a thick and prominent nipple.`,
-//       `{{C::character.firstName}} has six rows of small conical ${_tits()}. Each of {{C::gender.his}} ${_tits()} is capped by a prominent nipple almost as large as the ${_tit()} itself.`,
-//     ]); }
-//
-//     if (tits.shape == 'perky' && tits.sizeClass == 'tiny') { return Random.from([
-//       `{{C::character.firstName's}} ${_tits()} are tiny and perky, with prominent upturned nipples set high on each of {{C::gender.his}} ${_tits()}.`,
-//       `{{C::character.firstName's}} chest is adorned with twelve tiny ${_tits()}. Each of {{C::gender.his}} perky upturned ${_tits()} is too small to do much more than push {{C::gender.his}} many prominent nipples out just a bit further.`,
-//       `{{C::character.firstName}} has six rows of tiny ${_tits()}. {{C::gender.His}} twelve prominent nipples are set high on each of {{C::gender.his}} perky tits, making them look even more jutting and pinchable.`
-//     ]); }
-//
-//     if (tits.shape == 'perky' && tits.sizeClass == 'small') { return Random.from([
-//       `{{C::character.firstName's}} ${_tits()} are small and perky, with prominent upturned nipples set high on each of {{C::gender.his}} ${_tits()}.`,
-//       `{{C::character.firstName's}} chest is adorned with twelve small ${_tits()}. Each of {{C::gender.his}} perky upturned ${_tits()} pushes {{C::gender.his}} prominent nipples upward proudly.`,
-//       `{{C::character.firstName}} has six rows of small ${_tits()}. {{C::gender.His}} twelve prominent nipples are set high on each of {{C::gender.his}} perky tits, making them look even more jutting and pinchable.`
-//     ]); }
-//
-//     if (tits.shape == 'round' && tits.sizeClass == 'tiny') { return Random.from([
-//       `{{C::character.firstName's}} ${_tits()} are round but tiny. Each of {{C::gender.his}} tiny round orbs is slightly smaller than a lemon and capped with a thick prominent nipple.`,
-//       `{{C::character.firstName's}} chest is studded with twelve tiny ${_tits()}. Each of {{C::gender.his}} round ${_tits()} are slightly smaller than a lemon and capped with a thick prominent nipple.`,
-//       `{{C::character.firstName}} has twelve round but tiny ${_tits()}. Each of {{C::gender.his}} tits are slightly smaller than a lemon and capped with a thick prominent nipple.`,
-//     ]); }
-//
-//     if (tits.shape == 'round' && tits.sizeClass == 'small') { return Random.from([
-//       `{{C::character.firstName's}} ${_tits()} are small and round. Each of {{C::gender.his}} round ${_tits()} is slightly larger than a lemon and capped with a thick prominent nipple.`,
-//       `{{C::character.firstName's}} chest is studded with twelve small ${_tits()}. Each of {{C::gender.his}} round ${_tits()} are slightly larger than a lemon and capped with a thick prominent nipple.`,
-//       `{{C::character.firstName}} has twelve small round ${_tits()}. Each of {{C::gender.his}} tits are slightly larger than a lemon and capped with a thick prominent nipple.`,
-//     ]); }
-//
-//     return Weaver.error(`TODO: Describe rat tits of larger sizes. Shape:${tits.shape} Size:${tits.sizeClass}`)
-//   }
 //
 //   // Get general nipple descriptions.
 //   function describeNipples(character,nipples) {
@@ -268,7 +199,7 @@ global.TitsDescriber = class TitsDescriber {
 //       ArrayUtility.addAll(pile,[
 //         `{{C::gender.He}} has {{C::nipples.large}} {{C::nipples.color}} puffy nipples that are nearly as long as they are wide.`,
 //         `{{C::gender.He}} has {{C::nipples.large}} puffy nipples the size of {{C::nipples.grapes}}.`,
-//         `{{C::gender.His}} {{C::nipples.large}} {{C::nipples.color}} puffy nipples prodrude far from the surface of {{C::gender.his}} ${_tits()}.`,
+//         `{{C::gender.His}} {{C::nipples.large}} {{C::nipples.color}} puffy nipples prodrude far from the surface of {{C::gender.his}} {{tits}}.`,
 //         `{{C::gender.His}} puffy {{C::nipples.color}} nipples are about {{C::nipples.width}} wide, and nearly as long.`,
 //         `{{C::gender.His}} puffy {{C::nipples.color}} nipples are about {{C::nipples.length}} long and are as thick as {{C::nipples.grapes}}.`,
 //       ]);
