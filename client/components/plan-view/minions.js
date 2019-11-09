8c4acecaf36e14ba5c1f218896d573d8d3a9d0fe
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-11-09 11:45:23 -0500

    Only include free minions in plan

--- client/components/plan-view/minions.js
+++ client/components/plan-view/minions.js
@@ -5,7 +5,9 @@ Components.PlanView.Minions = (function() {
   }
 
   function build(planData) {
-    Components.MinionListView.buildForPlan($('#planView .plan-minion-list'), planData.minions, addRoleSelect);
+    Components.MinionListView.buildForPlan($('#planView .plan-minion-list'), planData.minions.filter(minion => {
+      return minion.currentDuty == 'role'
+    }), addRoleSelect);
   }
 
   function getFree() {
@@ -27,8 +29,7 @@ Components.PlanView.Minions = (function() {
   // The claim() functions sets the currentDuty for each of the minions in the
   // minion ID list and also marks the minion as taken in the UI.
   function claim(minionIDs, duty) {
-    let minionData = Components.PlanView.getPlanData().minions;
-    each(minionData, minion => {
+    each(Components.PlanView.getPlanData().minions, minion => {
       if (minionIDs.indexOf(minion.id) >= 0) {
         minion.currentDuty = duty;
         $(`.plan-minion-list .minion-${minion.id}`).removeClass('free').addClass('taken');
