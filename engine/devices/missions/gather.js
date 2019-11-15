478e198e074d149b908bec3ffd0866ce4633a798
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-11-14 20:27:02 -0500

    Fix some report bugs first

--- engine/devices/missions/gather.js
+++ engine/devices/missions/gather.js
@@ -2,30 +2,31 @@ Mission.Gather = (function() {
 
   // TODO: This will eventually get fairly complicated, but for the time being
   //       The gathering missions will need to stay super simple. What is
-  //       gathered is based on the teir, which is based on a minion's
-  //       equipment and perhaps some other factors as well. Nothing like that
-  //       is implemented yet though, so we're just doing a very basic
-  //       implementation here. This will need redone completely.
+  //       gathered is based on each character's equipment tier and perhaps
+  //       some other factors as well. Nothing like that is implemented yet
+  //       though, so we're just doing a very basic implementation here. This
+  //       will need redone completely.
   //
-  // input:  { mission
-  //           state
-  //           minion }
+  // input:  { mission, state, minion }
   //
-  // output: { story
-  //           flavors
-  //           items
-  //           notifications
-  //           injury }
   function resolve(data) {
-    let temp = data.mission.tempResults;
-    let flavors = {};
-        flavors[temp.flavor] = Random.between(temp.min, temp.max);
+    each(data.minions, minion => {
+      resolveMinion(data.mission, minion)
+    });
+  }
 
-    return {
-      story: "[TODO: Gather Mission Story]",
-      flavors: ItemFlavor.forReport(flavors),
-      items: ItemFlavor.itemize(flavors),
-    }
+  // Resolve the mission for this minion.
+  function resolveMinion(mission, minion) {
+    let results = mission.tempResults;
+
+    let gathered = {};
+        gathered[results.flavor] = Random.between(results.min, results.max);
+
+    Resolver.Items.add(ItemFlavor.itemize(gathered));
+    Resolver.Report.setMinionData(minion, 'work', {
+      story: `[TODO: ${mission.code} mission story]`,
+      flavors: ItemFlavor.forReport(gathered),
+    });
   }
 
   return { resolve };
