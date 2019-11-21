d518ac9c02b0a77c7d87012bc23dbb138a84d0ff
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-11-20 20:57:26 -0500

    Add image resource loader

---
+++ engine/devices/image-resource-loader.js
@@ -0,0 +1,15 @@
+global.ImageResourceLoader = (function() {
+
+  // type: background
+  // location
+  // locationState
+  // time
+  async function lookup(desires) {
+
+  }
+
+  return {
+    lookup
+  };
+
+})();
