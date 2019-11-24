d2bba9922c937dd7bbe9f28241fc8f5acb53e98d
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-11-24 12:31:43 -0500

    Add test characters to spec test game

--- spec/game-helper.js
+++ spec/game-helper.js
@@ -1,23 +1,26 @@
 global.GameHelper = (function() {
 
-  function setupTestGame() {
-    let testPlayer = {
+  async function setupTestGame() {
+    let game = await Game.start();
+
+    let testCharacters = [
+      { type:'minion', species:'rat', gender:'male',   fear:Random.between(40,60), loyalty:Random.between(10,20) },
+      { type:'minion', species:'rat', gender:'female', fear:Random.between(40,60), loyalty:Random.between(10,20) },
+    ];
+
+    await Promise.all(testCharacters.map((options) => {
+      return CharacterBuilder.build(options);
+    }));
+
+    await game.createPlayer({
       title: "Master",
       firstName: "Testes",
       lastName: "Baggywrinkle",
       gender: "male",
       species: "caprien",
-    };
-
-    return new Promise(resolve => {
-      Game.start().then(game => {
-        Player.forge(testPlayer).then(resolve);
-      });
     });
   }
 
-  return {
-    setupTestGame: setupTestGame
-  }
+  return { setupTestGame };
 
 })();
