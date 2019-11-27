02d91da53893ec229faa31c53aae230a32178e0d
Maldrasen Des'Tempre <maldrasen@gmail.com>
2019-11-26 21:03:23 -0500

    Change how choosers are built.

--- data/artificer/events/debug-create-player.js
+++ data/artificer/events/debug-create-player.js
@@ -2,16 +2,15 @@ Event.build('debug-create-player', {
   background: { code:'void' },
 
   stages:[
-    {
-      chooserPage: true,
-      choices: Species.chooserOptions,
-      name: 'species',
-      text: "Choose the Player's species.",
-    },{
-      chooserPage: true,
-      choices: Gender.chooserOptions,
+    { chooserPage: true,
+      choices: state => { return Gender.chooserOptions(state); },
       name: 'gender',
       text: "Choose the Player's gender.",
+    },{
+      chooserPage: true,
+      choices: state => { return Species.chooserOptions(state); },
+      name: 'species',
+      text: "Choose the Player's species.",
     }
   ],
 
