af054020fd14d3ae0785fe79e97367dae8b66cee
Maldrasen <maldrasen@gmail.com>
2020-01-16 14:27:13 -0500

    Start ambush rat event

---
+++ data/faingorn/events/ambush-rat-setup.js
@@ -0,0 +1,14 @@
+Event.build('ambush-rat-setup', {
+  location: 'upper-keep',
+
+  stages:[{
+    pages:[
+      { text:'Ambush Rat Setup' },
+    ]
+  }],
+
+  onFinish: async () => {
+
+  },
+
+});
