6020de19eb29b30979752c67525ef77b784665c3
Maldrasen <maldrasen@gmail.com>
2020-01-21 17:45:11 -0500

    Add some items to forage for.

---
+++ data/artificer/item-flavors/foraging.js
@@ -0,0 +1,110 @@
+
+// === Food ===
+
+ItemFlavor.build('bitter-fruits', {
+  name: 'Bitter Fruits',
+  type: 'foraged-food',
+  foodValue: 2,
+  storyWord: 'bag of bitter fruits',
+  storyWords: 'bags of bitter fruits',
+  xp: 1,
+});
+
+ItemFlavor.build('goat-nuts', {
+  name: 'Goat Nuts',
+  type: 'foraged-food',
+  foodValue: 1,
+  storyWord: 'bag of goat nuts',
+  storyWords: 'bags of goat nuts',
+  xp: 1,
+});
+
+ItemFlavor.build('juice-berries', {
+  name: 'Juice Berries',
+  type: 'foraged-food',
+  foodValue: 1,
+  storyWord: 'bag of juice berries',
+  storyWords: 'bags of juice berries',
+  xp: 1,
+});
+
+ItemFlavor.build('sweet-fruits', {
+  name: 'Sweet Fruits',
+  type: 'foraged-food',
+  foodValue: 2,
+  storyWord: 'bag of sweet fruit',
+  storyWords: 'bags of sweet fruits',
+  xp: 2,
+});
+
+// === Herbs ===
+
+ItemFlavor.build('blood-berry', {
+  name: 'Blood Berries',
+  type: 'herb',
+  becomes: 'blood-berries',
+  becomesCount: 10,
+  storyWord: 'bag of blood berries',
+  storyWords: 'bags of blood berries',
+  xp: 1,
+});
+
+ItemFlavor.build('creeping-moss', {
+  name: 'Creeping Moss',
+  type: 'herb',
+  becomes: 'creeping-moss',
+  becomesCount: 4,
+  storyWord: 'handful of creeping moss',
+  storyWords: 'handfuls of creeping moss',
+  xp: 2,
+});
+
+ItemFlavor.build('firewasp', {
+  name: 'Firewasp Pepper',
+  type: 'herb',
+  becomes: 'firewasp',
+  becomesCount: 6,
+  storyWord: 'handful of firewasp peppers',
+  storyWords: 'handfuls of firewasp peppers',
+  xp: 6,
+});
+
+ItemFlavor.build('itchweed', {
+  name: 'Itchweed',
+  type: 'herb',
+  becomes: 'itchweed',
+  storyWord: 'itchweed flower',
+  storyWords: 'itchweed flowers',
+  becomesCount: 4,
+  xp: 4,
+});
+
+ItemFlavor.build('milkweed', {
+  name: 'Milkweed',
+  type: 'herb',
+  becomes: 'milkweed',
+  storyWord: 'milkweed flower',
+  storyWords: 'milkweed flowers',
+  becomesCount: 4,
+  xp: 4,
+});
+
+ItemFlavor.build('purple-mushroom', {
+  name: 'Purple Headed Donkey',
+  type: 'herb',
+  becomes: 'purple-mushroom',
+  becomesCount: 1,
+  storyWord: 'purple headed donkey',
+  storyWords: 'purple headed donkeys',
+  xp: 4,
+});
+
+// === Insects ===
+
+ItemFlavor.build('blight-spider', {
+  name: 'Harry Bag Spider',
+  type: 'insect',
+  becomes: 'blight-spider',
+  becomesCount: 1,
+  xp: 10,
+});
---
+++ data/artificer/items/alchemy.js
@@ -0,0 +1,25 @@
+
+Item.build('milkweed', {
+  type: 'resource',
+  name: 'Milkweed',
+});
+
+Item.build('itchweed', {
+  type: 'resource',
+  name: 'Itchweed',
+});
+
+Item.build('firewasp', {
+  type: 'resource',
+  name: 'Firewasp Pepper',
+});
+
+Item.build('blight-spider', {
+  type: 'resource',
+  name: 'Harry Bag Spider',
+});
+
+Item.build('purple-mushroom', {
+  type: 'resource',
+  name: 'Purple Headed Donkey',
+});
