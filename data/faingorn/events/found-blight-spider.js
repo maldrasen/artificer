65ba48e5c63e6751e54336fcac08895ec2d6b97e
Maldrasen <maldrasen@gmail.com>
2020-01-23 10:31:26 -0500

    Add found events

---
+++ data/faingorn/events/found-blight-spider.js
@@ -0,0 +1,9 @@
+Event.build('found-blight-spider', {
+  background:{ location:'great-hall', time:'evening' },
+
+  stages:[{
+    pages:[
+      { text:'TODO: Found Blight Spider.' }
+    ]
+  }]
+});
---
+++ data/faingorn/events/found-blood-berries.js
@@ -0,0 +1,9 @@
+Event.build('found-blood-berries', {
+  background:{ location:'great-hall', time:'evening' },
+
+  stages:[{
+    pages:[
+      { text:'TODO: Found Blood Berries.' }
+    ]
+  }]
+});
