df7a70efe35b772824316d7453320a8e719b95d1
Maldrasen <maldrasen@gmail.com>
2020-01-24 14:44:49 -0500

    Add a foraging hazard.

---
+++ data/faingorn/hazards/foraging-hazards.js
@@ -0,0 +1,5 @@
+
+Hazard.buildHazard({ activity:'foraging-hinterlands', location:'body', type:'cut', level:1,
+  details: { place:'left-arm' },
+  story: `{{F::gender.He}} got a nasty scrape along {{F::gender.his}} left arm when {{F::gender.he}} tripped and fell against a jagged rock.`
+});
