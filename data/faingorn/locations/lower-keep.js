ed9be304eddf05256be22fcc9482499a0dca5c67
Maldrasen <maldrasen@gmail.com>
2020-02-01 22:01:50 -0500

    Updaing Descriptions

--- data/faingorn/locations/lower-keep.js
+++ data/faingorn/locations/lower-keep.js
@@ -5,7 +5,10 @@ Location.build('lower-keep', {
   },
 
   buildDescription: async () => {
-    return `TODO: Describe lower keep.`;
+    return `I've started referring to the keep's expansive first floor as the lower keep. The lower keep is perhaps as
+      large as all the rooms that make up the keep's central tower put together. The lower keep consists of the great
+      hall, the servent's quarters, an old smithy, and the barracks; all of which are arranged around the hexagonal
+      central courtyard. For the time being though, I really have no use for all this extra space.`;
   },
 
 });
--- data/faingorn/locations/upper-keep.js
+++ data/faingorn/locations/upper-keep.js
@@ -5,7 +5,10 @@ Location.build('upper-keep', {
   },
 
   buildDescription: async () => {
-    return `TODO: Describe upper keep.`;
+    return `The keep has a single five story tall tower that I've come to call the upper keep. I don't venture up here
+      very often though, and I really don't have much use for this space yet. Because of the tower's collapsed roof,
+      the upper keep has seen the most damage from both water and animals and will take quite a lot of work to make
+      livable again.`;
   },
 
 });
