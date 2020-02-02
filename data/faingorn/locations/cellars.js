f06a002f50de871b674c44b01ac2ac816b419ad2
Maldrasen <maldrasen@gmail.com>
2020-02-01 22:12:58 -0500

    Update descriptions.

--- data/faingorn/locations/cellars.js
+++ data/faingorn/locations/cellars.js
@@ -5,7 +5,11 @@ Location.build('cellars', {
   },
 
   buildDescription: async () => {
-    return `TODO: Describe cellars.`;
+    return `The keep's cellars are composed of a dozen or so small chambers, arranged haphazardly and cut directly from
+      the bedrock on which the keep sits. Supposedly there's an entrance somewhere in these twisted passages that lead
+      even further down, into the warrens where the Deep Hole tribe dwells. I haven't investigated these cellars overly
+      much yet, just enough to confirm that any supplies down here have either already been scavenged or have rotted
+      away.`;
   },
 
 });
