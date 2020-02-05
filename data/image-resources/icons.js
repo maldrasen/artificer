859eda1fdf2f7b811d880f92ed5a8b124f2332cf
Maldrasen <maldrasen@gmail.com>
2020-02-05 09:46:35 -0500

    Add goat nuts icon

--- data/image-resources/icons.js
+++ data/image-resources/icons.js
@@ -3,6 +3,7 @@ ImageResource.buildIcon('icon-unknown',  { url:`${IMAGE_ROOT}/icons/_unknown.png
 
 ImageResource.buildIcon('icon-item-willow-branch',   { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
 
+ImageResource.buildIcon('icon-flavor-goat-nuts', { url:`${IMAGE_ROOT}/icons/goat-nuts.png` });
 ImageResource.buildIcon('icon-flavor-willow-branches', { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
 
 // Items are automatically found by form class and code.
