b791eb847761cf30ce0b6676e4ccf38387fddd33
Maldrasen <maldrasen@gmail.com>
2020-02-15 14:47:51 -0500

    Juice berry icon

--- data/image-resources/icons.js
+++ data/image-resources/icons.js
@@ -1,4 +1,9 @@
 
+// Item icons are automatically found by form class and code.
+//   icon-item-{code}
+//   icon-flavor-{code}
+//   icon-equipment-{code}
+
 ImageResource.buildIcon('icon-unknown',  { url:`${IMAGE_ROOT}/icons/unknown.png` });
 
 ImageResource.buildIcon('icon-equipment-empty', { url:`${IMAGE_ROOT}/icons/empty.png` });
@@ -6,11 +11,7 @@ ImageResource.buildIcon('icon-equipment-basket', { url:`${IMAGE_ROOT}/icons/bask
 
 ImageResource.buildIcon('icon-item-willow-branch',   { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
 
-ImageResource.buildIcon('icon-flavor-goat-nuts', { url:`${IMAGE_ROOT}/icons/goat-nuts.png` });
-ImageResource.buildIcon('icon-flavor-willow-branches', { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
 ImageResource.buildIcon('icon-flavor-bitter-fruits', { url:`${IMAGE_ROOT}/icons/bitter-fruits.png` });
-
-// Items are automatically found by form class and code.
-//   icon-item-{code}
-//   icon-flavor-{code}
-//   icon-equipment-{code}
+ImageResource.buildIcon('icon-flavor-goat-nuts', { url:`${IMAGE_ROOT}/icons/goat-nuts.png` });
+ImageResource.buildIcon('icon-flavor-juice-berries', { url:`${IMAGE_ROOT}/icons/juice-berries.png` });
+ImageResource.buildIcon('icon-flavor-willow-branches', { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
