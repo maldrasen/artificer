6a98287e6810e63c2f89eb6748a9cf2e8473289a
Maldrasen <maldrasen@gmail.com>
2020-02-16 10:24:17 -0500

    Update icons

--- data/image-resources/icons.js
+++ data/image-resources/icons.js
@@ -12,6 +12,7 @@ ImageResource.buildIcon('icon-equipment-basket', { url:`${IMAGE_ROOT}/icons/bask
 ImageResource.buildIcon('icon-item-willow-branch',   { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
 
 ImageResource.buildIcon('icon-flavor-bitter-fruits', { url:`${IMAGE_ROOT}/icons/bitter-fruits.png` });
+ImageResource.buildIcon('icon-flavor-blood-berries', { url:`${IMAGE_ROOT}/icons/blood-berries.png` });
 ImageResource.buildIcon('icon-flavor-goat-nuts', { url:`${IMAGE_ROOT}/icons/goat-nuts.png` });
 ImageResource.buildIcon('icon-flavor-juice-berries', { url:`${IMAGE_ROOT}/icons/juice-berries.png` });
 ImageResource.buildIcon('icon-flavor-willow-branches', { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
