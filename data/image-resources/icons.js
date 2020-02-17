9b61342611b9ba255ff30409797fd0ac31d3444e
Maldrasen <maldrasen@gmail.com>
2020-02-17 12:54:34 -0500

    Sweet fruit icon

--- data/image-resources/icons.js
+++ data/image-resources/icons.js
@@ -4,16 +4,17 @@
 //   icon-flavor-{code}
 //   icon-equipment-{code}
 
-ImageResource.buildIcon('icon-unknown',  { url:`${IMAGE_ROOT}/icons/unknown.png` });
+ImageResource.buildIcon('icon-unknown', { url:`${IMAGE_ROOT}/icons/unknown.png` });
 
-ImageResource.buildIcon('icon-equipment-empty', { url:`${IMAGE_ROOT}/icons/empty.png` });
+ImageResource.buildIcon('icon-equipment-empty',  { url:`${IMAGE_ROOT}/icons/empty.png` });
 ImageResource.buildIcon('icon-equipment-basket', { url:`${IMAGE_ROOT}/icons/basket.png` });
 
 ImageResource.buildIcon('icon-item-blood-berries', { url:`${IMAGE_ROOT}/icons/blood-berries.png` });
-ImageResource.buildIcon('icon-item-willow-branch',   { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
+ImageResource.buildIcon('icon-item-willow-branch', { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
 
-ImageResource.buildIcon('icon-flavor-bitter-fruits', { url:`${IMAGE_ROOT}/icons/bitter-fruits.png` });
-ImageResource.buildIcon('icon-flavor-blood-berries', { url:`${IMAGE_ROOT}/icons/blood-berries.png` });
-ImageResource.buildIcon('icon-flavor-goat-nuts', { url:`${IMAGE_ROOT}/icons/goat-nuts.png` });
-ImageResource.buildIcon('icon-flavor-juice-berries', { url:`${IMAGE_ROOT}/icons/juice-berries.png` });
+ImageResource.buildIcon('icon-flavor-bitter-fruits',   { url:`${IMAGE_ROOT}/icons/bitter-fruits.png` });
+ImageResource.buildIcon('icon-flavor-blood-berries',   { url:`${IMAGE_ROOT}/icons/blood-berries.png` });
+ImageResource.buildIcon('icon-flavor-goat-nuts',       { url:`${IMAGE_ROOT}/icons/goat-nuts.png`     });
+ImageResource.buildIcon('icon-flavor-juice-berries',   { url:`${IMAGE_ROOT}/icons/juice-berries.png` });
+ImageResource.buildIcon('icon-flavor-sweet-fruits',    { url:`${IMAGE_ROOT}/icons/sweet-fruits.png`  });
 ImageResource.buildIcon('icon-flavor-willow-branches', { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
