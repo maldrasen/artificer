5e8893b20541e8c0d3f5a70992e1856d93f51ba0
Maldrasen <maldrasen@gmail.com>
2020-02-21 10:49:41 -0500

    Move old event text

--- data/faingorn/events-redo/clear-upper-keep-done.js
+++
@@ -1,9 +0,0 @@
-Event.build('clear-upper-keep-done', {
-  background: { location:'study', locationState:'clean' },
-
-  stages:[{
-    pages:[
-      { text:`TODO: Clear upper keep done.` },
-    ]
-  }]
-});
