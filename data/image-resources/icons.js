33d9660c266623b55759a161e91dbc95b548a7d7
Maldrasen <maldrasen@gmail.com>
2020-02-21 22:21:18 -0500

    Add spider icon

--- data/image-resources/icons.js
+++ data/image-resources/icons.js
@@ -9,10 +9,12 @@ ImageResource.buildIcon('icon-unknown', { url:`${IMAGE_ROOT}/icons/unknown.png` 
 ImageResource.buildIcon('icon-equipment-empty',  { url:`${IMAGE_ROOT}/icons/empty.png` });
 ImageResource.buildIcon('icon-equipment-basket', { url:`${IMAGE_ROOT}/icons/basket.png` });
 
+ImageResource.buildIcon('icon-item-blight-spider', { url:`${IMAGE_ROOT}/icons/blight-spider.png` });
 ImageResource.buildIcon('icon-item-blood-berries', { url:`${IMAGE_ROOT}/icons/blood-berries.png` });
 ImageResource.buildIcon('icon-item-willow-branch', { url:`${IMAGE_ROOT}/icons/willow-branch.png` });
 
 ImageResource.buildIcon('icon-flavor-bitter-fruits',   { url:`${IMAGE_ROOT}/icons/bitter-fruits.png` });
+ImageResource.buildIcon('icon-flavor-blight-spider',   { url:`${IMAGE_ROOT}/icons/blight-spider.png` });
 ImageResource.buildIcon('icon-flavor-blood-berries',   { url:`${IMAGE_ROOT}/icons/blood-berries.png` });
 ImageResource.buildIcon('icon-flavor-goat-nuts',       { url:`${IMAGE_ROOT}/icons/goat-nuts.png`     });
 ImageResource.buildIcon('icon-flavor-juice-berries',   { url:`${IMAGE_ROOT}/icons/juice-berries.png` });
