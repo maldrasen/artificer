713fb7c599224f0fc662d2a6a7926232feec51bb
Maldrasen <maldrasen@gmail.com>
2020-03-05 11:49:59 -0500

    Remove sensitivity aspects

--- data/artificer/aspects/sexual/anal-insensitive.js
+++
@@ -1,8 +0,0 @@
-Aspect.build('anal-insensitive', {
-  name: 'Anal Insensitive',
-  type: 'sexual',
-  description: 'is unlikely to orgasm from anal sex, but is better and handling anal pain.',
-  refutes: [
-    { aspect:'anal-sensitive' },
-  ]
-});
