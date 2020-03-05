f8350b397f36724949225d19d33a963321f03356
Maldrasen <maldrasen@gmail.com>
2020-03-05 11:44:15 -0500

    Remove gently and intimated

--- data/artificer/aspects/personality/gentle.js
+++
@@ -1,9 +0,0 @@
-Aspect.build('gentle', {
-  name: 'Gentle',
-  type: 'personality',
-  description: 'does not enjoy hurting others.',
-  refutes: [
-    { aspect:'voilent' },
-    { aspect:'sadist' }
-  ]
-});
--- data/artificer/aspects/personality/intimated.js
+++
@@ -1,5 +0,0 @@
-Aspect.build('intimated', {
-  name: 'Intimated',
-  type: 'personality',
-  description: 'is intimated by the player.',
-});
