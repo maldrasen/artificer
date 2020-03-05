0e57797dd5d8d200881f903b9dcfcee128bec991
Maldrasen <maldrasen@gmail.com>
2020-03-05 12:10:42 -0500

    Update anal aspects

---
+++ data/artificer/aspects/sexual/anal-averse.js
@@ -0,0 +1,8 @@
+Aspect.build('anal-averse', {
+  name: 'Anal Averse',
+  type: 'sexual',
+  description: "doesn't like having their asshole touched, much less penetrated.",
+  refutes: [
+    { aspect:'anal-slut' }
+  ]
+});
