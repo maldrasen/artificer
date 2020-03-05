9f6cc5757ee2f34d7be5940169c30e0e10f4bbc6
Maldrasen <maldrasen@gmail.com>
2020-03-05 12:58:04 -0500

    Get rid of shameless

--- data/artificer/aspects/sexual/shameless.js
+++
@@ -1,8 +0,0 @@
-Aspect.build('shameless', {
-  name: 'Shameless',
-  type: 'sexual',
-  description: 'is hardly ever embarrassed by anything.',
-  refutes: [
-    { aspect:'shy' }
-  ]
-});
--- data/artificer/aspects/sexual/shy.js
+++
@@ -1,8 +0,0 @@
-Aspect.build('shy', {
-  name: 'Shy',
-  type: 'sexual',
-  description: 'is embarrassed by sex and nudity.',
-  refutes: [
-    { aspect:'shameless' },
-  ]
-});
