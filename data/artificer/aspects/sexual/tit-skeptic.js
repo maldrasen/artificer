00663a0b816c13ce76f5c32e140389fd6a25ca60
Maldrasen <maldrasen@gmail.com>
2020-03-05 13:01:23 -0500

    Get rid of tit shy and skeptic

--- data/artificer/aspects/sexual/tit-skeptic.js
+++
@@ -1,9 +0,0 @@
-Aspect.build('tit-skeptic', {
-  name: 'Tit Skeptic',
-  type: 'sexual',
-  description: 'is not sexually aroused by breasts.',
-  refutes: [
-    { aspect:'gynephilic' },
-    { aspect:'tit-lover' },
-  ]
-});
