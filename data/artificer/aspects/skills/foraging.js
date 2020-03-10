d763227a0d928a52bc1ed11c330eb68f2a944bbc
Maldrasen <maldrasen@gmail.com>
2020-03-10 17:19:39 -0400

    Add names

--- data/artificer/aspects/skills/foraging.js
+++ data/artificer/aspects/skills/foraging.js
@@ -1,4 +1,5 @@
 Aspect.build('foraging', {
+  name: 'Foraging',
   type: 'skill',
   attribute: 'physical',
   description: "The foraging skill determines how well a character is at finding food and useful plants in the wild.",
--- data/artificer/aspects/skills/oral.js
+++ data/artificer/aspects/skills/oral.js
@@ -1,4 +1,5 @@
 Aspect.build('oral', {
+  name: 'Oral',
   type: 'skill',
   attribute: 'physical',
   description: "The oral skill determines how well a character is at pleasuring others with their mouth.",
