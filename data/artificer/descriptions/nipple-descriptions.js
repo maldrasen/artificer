5fc469cb88ad2dd0731181546f4d5c2aa97e66a7
Maldrasen <maldrasen@gmail.com>
2020-03-21 21:04:55 -0400

    Use shortcuts

--- data/artificer/descriptions/nipple-descriptions.js
+++ data/artificer/descriptions/nipple-descriptions.js
@@ -1,58 +1,53 @@
 
 Description.buildNipples({
-  d: `{{C::gender.He}} has {{C::nipples.large}} {{C::nipples.color}} nipples the size of {{C::nipples.grapes}}.`,
+  d: `{{He}} has {{C::nipples.large}} {{C::nipples.color}} nipples the size of {{C::nipples.grapes}}.`,
 });
 
 Description.buildNipples({
-  d: `{{C::gender.He}} has {{C::nipples.large}} {{C::nipples.color}} nipples.`,
+  d: `{{He}} has {{C::nipples.large}} {{C::nipples.color}} nipples.`,
 });
 
 Description.buildNipples({
-  d: `{{C::gender.His}} {{C::nipples.large}} {{C::nipples.color}} nipples are about {{C::nipples.width}} wide.`,
+  d: `{{His}} {{C::nipples.large}} {{C::nipples.color}} nipples are about {{C::nipples.width}} wide.`,
 });
 
 Description.buildNipples({
-  d: `{{C::gender.His}} {{C::nipples.large}} {{C::nipples.color}} nipples are the size of {{C::nipples.grapes}}.`,
+  d: `{{His}} {{C::nipples.large}} {{C::nipples.color}} nipples are the size of {{C::nipples.grapes}}.`,
 });
 
 Description.buildNipples({
-  d: `{{C::gender.His}} {{C::nipples.thickNipples}} are about {{C::nipples.width}} wide with tips the size of {{C::nipples.grapes}}.`,
+  d: `{{His}} {{C::nipples.thickNipples}} are about {{C::nipples.width}} wide with tips the size of {{C::nipples.grapes}}.`,
 });
 
 // === Furry ===
 
 Description.buildNipples({ requirements:['species-furry','nipples-size-big'],
-  d: `{{C::gender.His}} {{C::nipples.large}} {{C::nipples.color}} nipples protrude noticibly from {{C::gender.his}} fur
-      covered chest.`
+  d: `{{His}} {{C::nipples.large}} {{C::nipples.color}} nipples protrude noticibly from {{his}} fur covered chest.`
 });
 
 Description.buildNipples({ requirements:['species-furry','nipples-size-big'],
-  d: `{{C::gender.He}} has {{C::nipples.large}} {{C::nipples.grape}} sized nipples that extend well past
-      {{C::gender.his}} chest fur.`,
+  d: `{{He}} has {{C::nipples.large}} {{C::nipples.grape}} sized nipples that extend well past {{his}} chest fur.`,
 });
 
 // Shape - Puffy
 
 Description.buildNipples({ requirements:['nipples-shape-puffy'],
-  d: `{{C::gender.He}} has {{C::nipples.large}} {{C::nipples.color}} puffy nipples that are nearly as long as they are
-      wide.`,
+  d: `{{He}} has {{C::nipples.large}} {{C::nipples.color}} puffy nipples that are nearly as long as they are wide.`,
 });
 
 Description.buildNipples({ requirements:['nipples-shape-puffy'],
-  d: `{{C::gender.He}} has {{C::nipples.large}} puffy nipples the size of {{C::nipples.grapes}}.`,
+  d: `{{He}} has {{C::nipples.large}} puffy nipples the size of {{C::nipples.grapes}}.`,
 });
 
 
 Description.buildNipples({ requirements:['nipples-shape-puffy'],
-  d: `{{C::gender.His}} puffy {{C::nipples.color}} nipples are about {{C::nipples.width}} wide, and nearly as long.`,
+  d: `{{His}} puffy {{C::nipples.color}} nipples are about {{C::nipples.width}} wide, and nearly as long.`,
 });
 
 Description.buildNipples({ requirements:['nipples-shape-puffy'],
-  d: `{{C::gender.His}} puffy {{C::nipples.color}} nipples are about {{C::nipples.length}} long and are as thick as
-      {{C::nipples.grapes}}.`,
+  d: `{{His}} puffy {{C::nipples.color}} nipples are about {{C::nipples.length}} long and are as thick as {{C::nipples.grapes}}.`,
 });
 
 Description.buildNipples({ requirements:['tits-size-below-average','nipples-shape-puffy'],
-  d: `{{C::gender.His}} {{C::nipples.large}} {{C::nipples.color}} puffy nipples prodrude far from the surface of
-      {{C::gender.his}} {{tits}}.`,
+  d: `{{His}} {{C::nipples.large}} {{C::nipples.color}} puffy nipples prodrude far from the surface of {{his}} {{tits}}.`,
 });
