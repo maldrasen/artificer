c8fdd84de8d44b1d585de1a606407ba5d29b08cb
Maldrasen <maldrasen@gmail.com>
2020-03-24 09:56:53 -0400

    Improve the randomization in the spec

--- spec/summon-action-helper.js
+++ spec/summon-action-helper.js
@@ -15,6 +15,8 @@ global.SummonActionHelper = (function() {
       species: (scene.player.species || 'elf'),
     });
 
+    await adjustPlayer(scene.player);
+
     if (scene.character.gender == null) {
       scene.character.gender = Random.fromFrequencyMap({ male:7, futa:3, female:10 });
     }
@@ -22,13 +24,69 @@ global.SummonActionHelper = (function() {
     return await SpecHelper.buildJada(scene.character);
   }
 
-  async function buildAndExecute(character, action) {
-    const summoner = new Summoner(character.id, action);
-    await summoner.init();
-    await summoner.execute();
-    return summoner.getResult().story;
+  // We normally don't use the character builder for the player, and adding all
+  // the conditionals and crap to make this work would just be nasty, so if we
+  // want to set player attributes in the scene we just do it by hand here.
+  async function adjustPlayer(options) {
+    const player = await Player.instance();
+
+    if (options.body) {
+      const body = await player.getBody();
+      if (options.body.height) { body.height = options.body.height; }
+      await body.save();
+    }
+
+    if (options.cock) {
+      const cock = await player.getCock();
+      if (options.cock.sizeClass) { cock.sizeClass = options.cock.sizeClass; }
+      if (options.cock.sizeScale) { cock.sizeScale = options.cock.sizeScale; }
+      await cock.save();
+    }
   }
 
-  return { setupScene, buildAndExecute };
+  // When we take our shotgun approach to testing these summon actions we want
+  // to randomize the scene enough to generate a lot of variety in the tests.
+  // It's possible though to create characters that can no longer perform the
+  // action under test, so we need to check the requirements of each action
+  // before building the scene.
+  async function buildAndExecute(character, action) {
+    const context = new WeaverContext();
+    await context.addCharacter('C',character);
+    await context.addPlayer();
+
+    if (await CentralScrutinizer.meetsRequirements(SummonAction.lookup(action).requirements, context)) {
+      const summoner = new Summoner(character.id, action);
+      await summoner.init();
+      await summoner.execute();
+      return summoner.getResult().story;
+    } else {
+      SpecHelper.print(`Preconditions failed, rejected scene.`);
+    }
+  }
+
+  function randomSpecies() {
+    return Random.from(Species.all()).code;
+  }
+
+  function randomPlayableSpecies() {
+    return Random.from(['caprien','dark-elf','elf','equian','lupin','minotaur','wood-elf']);
+  }
+
+  // Generate a super random cock in order to add more variety to the scenes
+  // able to be seelcted.
+  function randomCockProperties() {
+    return {
+      sizeClass: Random.from(['average','big','huge']),
+      sizeScale: Random.between(1,100),
+    }
+  }
+
+  return {
+    setupScene,
+    buildAndExecute,
+    randomSpecies,
+    randomPlayableSpecies,
+    randomCockProperties
+  };
 
 })();
--- spec/summon-actions/get-blowjob-spec.js
+++ spec/summon-actions/get-blowjob-spec.js
@@ -1,16 +1,42 @@
 describe.only('Summon Action: Get Blowjob', function() {
 
+  function defaultScene() {
+    return {
+      player: {
+        gender: 'male',
+        species: SummonActionHelper.randomPlayableSpecies(),
+        cock: SummonActionHelper.randomCockProperties(),
+        body:{ height:1600 }
+      },
+      character:{
+        species: SummonActionHelper.randomSpecies(),
+        fear: 10,
+        loyalty: 50,
+        lust: 50,
+        body:{ height:1600 }},
+    }
+  }
+
+  // Test the blowjob scene with a tiny scaven. Needed to increase the player
+  // size, and reduce the scaven's size to make sure the size comparason ends
+  // up being possible, and needed to reduce the random cock sizes to make the
+  // action even possible most of the time.
   it('with tiny consenting scaven character', function(done) {
-    let scene = {
-      player:{ species:'minotaur', body:{ height:2500 }},
-      character:{ species:'scaven', fear:10, loyalty:50, lust:50, body:{ height:800 }},
-    };
+    let scene = extend(true, defaultScene(), {
+      player:{
+        body:{ height:2500 },
+        cock:{ sizeClass:Random.from(['small','average','big'])}
+      },
+      character:{ species:'scaven', body:{ height:800 }}
+    });
 
     SummonActionHelper.setupScene(scene).then(jada => {
       SpecHelper.print(``);
       SpecHelper.print(`==============================`);
       SpecHelper.print(`Tiny Consenting Scaven Blowjob`);
       SpecHelper.print(`==============================`);
+      SpecHelper.print(JSON.stringify(scene,null,2));
+      SpecHelper.print(``);
 
       SpecHelper.tenTimes(done, resolve => {
         SummonActionHelper.buildAndExecute(jada,'get-blowjob').then(story => {
