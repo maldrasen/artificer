f65adcda4407c92ef83e6389172af31429ab5e24
Maldrasen <maldrasen@gmail.com>
2020-04-09 15:11:02 -0400

    Reenable all specs

--- spec/describers/anus-injury-describer-spec.js
+++ spec/describers/anus-injury-describer-spec.js
@@ -1,6 +1,6 @@
 describe('Describer: Anus (injuries)', function() {
 
-  it.only('describes a whipped furry ass', function(done) {
+  it('describes a whipped furry ass', function(done) {
     SpecHelper.tenTimes(done, resolve => {
       SpecHelper.buildRando({ species:'scaven' }).then(jada => {
         Abuser.AnusAbuser.addInjury(jada, { type:'smash', level:Random.between(1,5), count:Random.between(1,12), details:{ shape:'whip' }}).then(anus => {
@@ -11,7 +11,7 @@ describe('Describer: Anus (injuries)', function() {
     });
   });
 
-  it.only('describes a whipped non-furry ass', function(done) {
+  it('describes a whipped non-furry ass', function(done) {
     SpecHelper.tenTimes(done, resolve => {
       SpecHelper.buildRando({ species:'elf' }).then(jada => {
         Abuser.AnusAbuser.addInjury(jada, { type:'smash', level:Random.between(1,5), count:Random.between(1,12), details:{ shape:'whip' }}).then(anus => {
