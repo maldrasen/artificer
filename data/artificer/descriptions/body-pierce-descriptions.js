a52093680a7ee62a7b96884ac835f781c70f758f
Maldrasen <maldrasen@gmail.com>
2020-04-13 22:28:22 -0400

    And finally, update a few pierce descriptions.

--- data/artificer/descriptions/body-pierce-descriptions.js
+++ data/artificer/descriptions/body-pierce-descriptions.js
@@ -1,8 +1,12 @@
 
 Description.buildBodyInjury({ damageType:'pierce', level:3, requires:['minion(C).body.pierce-count=1'],
-  d: `[TODO: Body Pierce 3, Single]`,
+  d: `It looks like {{he}}'s been stabbed in the gut. A painful, ragged looking wound is visible on {{his}} lower abdomen.`,
 });
 
-Description.buildBodyInjury({ damageType:'pierce', level:3, requires:['minion(C).body.pierce-count>1'],
-  d: `[TODO: Body Pierce 3, More than one]`,
+Description.buildBodyInjury({ damageType:'pierce', level:3, requires:['minion(C).body.pierce-count=2'],
+  d: `It looks like {{he}}'s been stabbed in the gut. A pair of painful, ragged looking wounds are visible on {{his}} lower abdomen.`,
+});
+
+Description.buildBodyInjury({ damageType:'pierce', level:3, requires:['minion(C).body.pierce-count>2'],
+  d: `It looks like {{he}}'s been stabbed several times in the gut. Painful, ragged looking wounds are visible on {{his}} lower abdomen.`,
 });
