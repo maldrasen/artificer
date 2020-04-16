8c900c1fed0b2dc450115bd536f18a445e839bfb
Maldrasen <maldrasen@gmail.com>
2020-04-16 18:17:21 -0400

    Update whipping event

--- data/faingorn/events/found-willow-branches-whip.js
+++ data/faingorn/events/found-willow-branches-whip.js
@@ -3,37 +3,40 @@ Event.build('found-willow-branches-whip', {
 
   stages:[{
     pages:[
-      { playerSpeaker:true, text:`I glance over at my rat, "You seem nervous {{C::character.firstName}}. Are whippings with branches like these common among your people?"` },
-      { actorSpeaker:'C', text:`{{C::gender.He}} sheepishly comes closer to me and says, "Yes, {{P::character.title}}. Very. The big scaven, they like to whip the smaller ones."` },
-      { text:`I bring the tip of the switch up to {{C::gender.his}} chest, gently flicking the tip over one of {{C::gender.his}} nipples.` },
-      { playerSpeaker:true, text:`"I see&hellip; and which were you?"` },
-      { playerSpeaker:true, text:`"Did you enjoy whipping your fellow scaven or were you the one getting whipped?"` },
+      { playerSpeaker:true, text:`I glance over at my rat, "You seem nervous {{C::character.firstName}}. Are whippings
+          with branches like these common among your people?"` },
+      { actorSpeaker:'C', text:`{{He}} sheepishly comes closer to me and says, "Yes, {{P::character.title}}. Very. The
+          big scaven, they like to whip the smaller ones."` },
+      { playerSpeaker:true, text:`I bring the tip of the switch up to {{his}} chest, gently flicking the tip over one
+          of {{his}} nipples. "I see&hellip; and which were you? Did you enjoy whipping your fellow scaven or were you
+          the one getting whipped?"` },
       { actorSpeaker:'C', text:`"Both."` },
-      { playerSpeaker:true, text:`"I see."` },
-      { playerSpeaker:true, text:`"And now that you're here, and because I'm so much larger than you are, you think that I'd like to whip you a bit myself?"` },
+      { playerSpeaker:true, text:`"I see. And now that you're here, and because I'm so much larger than you are, you
+          think that I'd like to whip you a bit myself?"` },
     ]
   },{
     requires:['minion(C).has-tits'],
-    pages:[{ text:`With a flick of the branch I open her vest up further and start rubbing the branch up and down her chest, brushing firmly against each of her hardening nipples in turn.` }],
+    pages:[{ text:`With a flick of the branch I open her vest up further and start rubbing the branch up and down her
+      chest, brushing firmly against each of her hardening nipples in turn.` }],
   },{
     requires:['minion(C).has-cock'],
-    pages:[{ text:`I trace a line down his chest with the tip down to his thigh, then raising it again I push the stick up under his loincloth and lift his ballsack gently with the tip of the branch.` }],
+    pages:[{ text:`I trace a line down his chest with the tip down to his thigh, then raising it again I push the stick
+      up under his loincloth and lift his ballsack gently with the tip of the branch.` }],
   },{
     pages:[
-      { actorSpeaker:'C', text:`{{C::gender.He}} inhales sharply as I toy with {{C::gender.him}}, "Yes, {{P::character.title}}."` },
-      { playerSpeaker:true, text:`I grin, "Well I'm glad we understand each other then."` },
-      { playerSpeaker:true, text:`"Tell me rat, where am I going to whip you?"` },
-      { text:`It's not a question {{C::gender.he}} was expecting.` },
-      { text:`I can almost see all the thoughts racing in {{C::gender.his}} head as {{C::gender.he}} tries to think of an answer that would be the least punishing.` },
-      { actorSpeaker:'C', text:`Finally {{C::gender.he}} stammers out, "M, My ass {{P::character.title}}?"` },
-      { playerSpeaker:true, text:`I nod slowly, "You know what you need to do then yes?"` },
-      { text:`{{C::gender.He}} nods and quickly gets naked, then climbs on top of one of the old wooden tables.` },
-      { text:`{{C::gender.He}} gets on {{C::gender.his}} hands and knees first, then lowers {{C::gender.his}} chest down onto the rough splintery surface, a position that pushes {{C::gender.his}} ass as high into the air as possible.` },
-      { text:`{{C::gender.His}} long naked tail whips back and forth nervously in the air above {{C::gender.him}}.` },
-      { text:`I walk around the table slowly.` },
-      { text:`Taking my time.` },
-      { text:`Letting the tension build while admiring {{C::gender.his}} upturned ass.` },
-      { text:`I reach down and take hold of {{C::gender.his}} knees suddenly, pulling them apart.` },
+      { actorSpeaker:'C', text:`{{He}} inhales sharply as I toy with {{him}}, "Yes, {{P::character.title}}."` },
+      { playerSpeaker:true, text:`I grin, "Well I'm glad we understand each other then. Tell me rat, where am I going
+          to whip you?"` },
+      { text:`It's not a question {{he}} was expecting. I can almost see all the thoughts racing in {{his}} head as
+          {{he}} tries to think of an answer that would be the least punishing.` },
+      { actorSpeaker:'C', text:`Finally {{he}} stammers, "M, My ass {{P::character.title}}?"` },
+      { playerSpeaker:true, text:`I nod slowly, "You know what you need to do then yes?" {{He}} nods and quickly gets
+          naked, then climbs on top of one of the old wooden tables. {{He}} gets up on {{his}} hands and knees first,
+          then lowers {{his}} chest down onto the rough splintery surface, a position that pushes {{his}} ass as high
+          into the air as possible.` },
+      { text:`{{His}} long naked tail whips back and forth nervously in the air above {{him}} as I walk around the
+          table slowly. Taking my time. Letting the tension build while admiring {{his}} upturned ass. I reach down and
+          take hold of {{his}} knees suddenly, pulling them apart.` },
     ]
   },{
     requires:['minion(C).has-tits'],
@@ -43,85 +46,88 @@ Event.build('found-willow-branches-whip', {
     pages:[{ text:`It lowers his ass a bit, but with his legs open it leaves his dangling ballsack completely exposed behind him.` }]
   },{
     pages:[
-      { text:`A position that's certain to make {{C::gender.him}} even more frightened.` },
-      { playerSpeaker:true, text:`"Now tell me... how many?"` },
-      { text:`The rat squirms on the tabletop, trying to come up with an acceptable answer.` },
-      { actorSpeaker:'C', text:`Finally {{C::gender.he}} asks, "Thirteen {{P::character.title}}?"` },
-      { playerSpeaker:true, text:`"Wrong!", I yell and suddenly slash at {{C::gender.his}} ass with the willow branch, landing a blow eavenly across both ass cheeks.`, effects:['actor(C) fear 1']},
-      { text:`{{C::gender.He}} howls and almost collapses against the tabletop but quickly recovers, returning to the ass up position.` },
-      { actorSpeaker:'C', text:`"Ahh! I don't know {{P::character.title}}."` },
-      { playerSpeaker:true, text:`"Count then!" I growl and hit {{C::gender.him}} again with the branch.`, effects:['actor(C) fear 1']},
-      { actorSpeaker:'C', text:`{{C::gender.He}} grunts as the branch slaps against {{C::gender.his}} ass, "Ahh! Two!"` },
-      { playerSpeaker:true, text:`"Do scaven start counting at two?"` },
-      { text:`I slash at {{C::gender.his}} ass again, this time landing a single blow vertically across {{C::gender.his}} left cheek.`, effects:['actor(C) fear 1']},
-      { playerSpeaker:true, text:`"Start over!"` },
+      { playerSpeaker:true, text:`It's a position that's certain to make {{him}} even more frightened. "Now tell
+          me&hellip; how many?"` },
+      { actorSpeaker:'C', text:`The rat squirms on the tabletop, trying to come up with an acceptable answer. Finally
+          {{he}} asks, "Thirteen {{P::character.title}}?"` },
+      { playerSpeaker:true, text:`"Wrong!", I yell and suddenly slash at {{his}} ass with the willow branch, landing a
+          blow eavenly across both ass cheeks.`, effects:['actor(C) fear 1']},
+      { actorSpeaker:'C', text:`{{He}} howls and almost collapses against the tabletop but quickly recovers, returning
+          to the ass up position. "Ahh! Don't know {{P::character.title}}."` },
+      { playerSpeaker:true, text:`"Count then!" I growl and hit {{him}} again with the branch.`, effects:['actor(C) fear 1']},
+      { actorSpeaker:'C', text:`{{He}} grunts as the branch slaps against {{his}} ass, "Ahh! Two!"` },
+      { playerSpeaker:true, text:`"Do scaven start counting at two?" I slash at {{his}} ass again, this time landing a
+          single blow vertically across {{his}} left cheek. "Start over!"`, effects:['actor(C) fear 1']},
       { actorSpeaker:'C', text:`"Ahh! One {{P::character.title}}!"` },
-      { text:`Again, a vertical slash against {{C::gender.his}} right cheek now.`, effects:['actor(C) fear 1']},
+      { text:`Again, a vertical slash against {{his}} right cheek now.`, effects:['actor(C) fear 1']},
       { actorSpeaker:'C', text:`"Aghh! Two {{P::character.title}}!"` },
       { text:`Finally another horizontal blow, dangerously low this time.`, effects:['actor(C) fear 1']},
       { actorSpeaker:'C', text:`"Ooah! Three {{P::character.title}}."` },
-      { text:`Finally, I place the branch down and start to massage {{C::gender.his}} ass, firmly rubbing the places I just beat {{C::gender.him}}.` },
-      { playerSpeaker:true, text:`"There&hellip; just three. Isn't that kind of me?"` },
-      { text:`{{C::gender.He}} groans and squirms in my grip, clearly {{C::gender.his}} ass is sore and rubbing just makes {{C::gender.him}} notice how sore {{C::gender.he}} really is.` },
-      { actorSpeaker:'C', text:`"Uhh. Yes {{P::character.title}}. Thank you {{P::character.title}}."` },
-      { text:`There's no blood.` },
-      { text:`Scaven fur is quite thick and shielded {{C::gender.him}} from the worst of it.` },
-      { text:`Under the fur though there's going to be some significant bruising.` },
+      { playerSpeaker:true, text:`Finally, I place the branch down and start to massage {{his}} ass, firmly rubbing the
+          places I just beat {{him}}. "There&hellip; just three. Isn't that kind of me?"` },
+      { actorSpeaker:'C', text:`{{He}} groans and squirms in my grip, clearly {{his}} ass is sore and rubbing just
+          makes {{him}} notice how sore {{he}} really is. "Uhh. Yes {{P::character.title}}. Thank you
+          {{P::character.title}}."` },
+      { text:`There's no blood. Scaven fur is quite thick and shielded {{him}} from the worst of it. Under the fur
+          though there's going to be some significant bruising.` },
     ]
   },{
     requires:['minion(C).has-tits'],
     pages:[
-      { text:`My hand roams lower, brushing against her swollen cunt.` },
-      { text:`The contact makes her gasp.` },
-      { playerSpeaker:true, text:`"Oh? And what's this?"` },
-      { text:`While still keeping one hand rubbing and squeezing her sore ass I press the other between her legs, letting my finger sink between her pussy lips to rest firmly against her clit.` },
+      { playerSpeaker:true, text:`My hand roams lower, brushing against her swollen cunt. The contact makes her gasp.
+          "Oh? And what's this?" While still keeping one hand rubbing and squeezing her sore ass I push the other
+          between her legs, letting my finger sink between her pussy lips to rest firmly against her clit. "You've got
+          an awfully wet pussy for a rat who's just had their ass whipped."` },
       { actorSpeaker:'C', text:`"Ahh&hellip; {{P::character.title}}&hellip; please?"` },
-      { text:`I slowly but firmly begin to rub her swollen clit, matching the movements of her hips as she pushes back against me.` },
-      { playerSpeaker:true, text:`"You want a reward little rat?"` },
-      { actorSpeaker:'C', text:`Softly she sighs, "Yes."` },
-      { text:`Her pussy is absolutely dripping, soaking the back of my hand as I toy with her clit.` },
-      { text:`I finally stop kneading her sore ass and move my other hand to her pussy as well, and then without preamble I firmly press a finger inside of her.` },
-      { text:`She squeals and arches her back at the sudden invasion.` },
-      { text:`My fingers pinch down on her clit, tugging on it, pulling her back onto my thrusting finger.` },
-      { text:`It only takes her a dozen or so thrusts but suddenly she's cumming, groaning, digging her claws into the wooden table as she thrusts back hard against my finger.` },
-      { text:`Finally, she collapses down onto the tabletop.` },
-      { text:`I let my finger slip out of her, though I do give her ass one last hard slap before I let her get away.` },
-      { text:`She responds with a high pitched squeak, followed by a giggle as she reaches behind to rub her sore ass.` },
-      { actorSpeaker:'C', text:`"Mmm, thank you {{P::character.title}}."` },
+      { playerSpeaker:true, text:`I slowly but firmly begin to rub her swollen clit, matching the movements of her hips
+          as she pushes back against me. "You want a reward little rat?"` },
+      { actorSpeaker:'C', text:`Her pussy is absolutely dripping, soaking the back of my hand as I toy with her clit.
+          Softly she sighs, "Yes."` },
+      { text:`I finally stop kneading her sore ass and move my other hand to her pussy as well, and then without
+          preamble I firmly shove a finger inside of her. She squeals and arches her back at the sudden invasion. My
+          fingers pinch down on her clit, tugging on it, pulling her back onto my thrusting finger.` },
+      { text:`It only takes her a dozen or so thrusts but suddenly she's cumming, groaning, digging her claws into the
+          wooden table as she thrusts back hard against my finger. Finally, she collapses down onto the tabletop. I let
+          my finger slip out of her, though I do give her ass one last hard slap before I let her get away.` },
+      { actorSpeaker:'C', text:`She responds with a high pitched squeak, followed by a giggle as she reaches behind to
+          rub her sore ass. "Mmm, thank you {{P::character.title}}."` },
     ]
   },{
     requires:['minion(C).has-cock'],
     pages:[
-      { text:`My hand roams lower, brushing against his tight ballsack.` },
-      { text:`The contact makes him gasp.` },
-      { text:`I trace a finger around his balls, then reaching under him I find that his cock has grown rock hard.` },
-      { playerSpeaker:true, text:`"Oh? And what's this?"` },
-      { text:`While massaging his sore ass, I gently start rubbing two fingers up and down his {{C::cock.sixInch}} long shaft, pinching its thick head between my fingers when I reach it.` },
-      { actorSpeaker:'C', text:`"Ahh&hellip; {{P::character.title}}&hellip; please?"` },
-      { text:`I finally wrap my fingers fully around his cock, squeezing it firmly though not rubbing quite yet.` },
-      { playerSpeaker:true, text:`"You want a reward little rat?"` },
+      { playerSpeaker:true, text:`My hand roams lower, brushing against his tight ballsack. The contact makes him gasp.
+          I trace a finger around his balls, then reaching under him I find that his cock has grown rock hard. "Oh?
+          What's this I'm feeling? You have an awfully hard cock for a rat who's just had their ass whipped."` },
+      { actorSpeaker:'C', text:`While massaging his sore ass, I gently start rubbing two fingers up and down his
+          {{C::cock.sixInch}} long shaft, pinching its thick head between my fingers when I reach it. "Ahh&hellip;
+          {{P::character.title}}&hellip; please?"` },
+      { playerSpeaker:true, text:`I finally wrap my fingers fully around his cock, squeezing it firmly though not
+          rubbing quite yet. "You want a reward little rat?"` },
       { actorSpeaker:'C', text:`Softly he sighs, "Yes."` },
-      { text:`I slowly start rubbing his cock, keeping my hand gripped tightly around it as he begins to thrust into my hand.` },
-      { text:`My other hand keeps squeezing and kneading his sore ass cheeks, reminding him of the soreness there to keep his recent beating in the forefront of his mind.` },
-      { text:`As I rub his ass my thumb strays closer to his ass crack, parting his cheeks while tugging at the side of his asshole.` },
-      { text:`As his thrusting begins to grow more urgent I relax my grip around his cock, giving him just enough grip to let him hump my hand freely while still giving him some friction.` },
-      { text:`My hand presses against his ass in time with his thrusting.` },
-      { text:`My thumb rests fully on his asshole, not entering him, just rubbing and pushing against the rim.` },
-      { text:`He doesn't last much longer.` },
-      { text:`As soon as I feel him tense up and his cock begins to twitch I push my thumb into his ass.` },
-      { text:`His cum soon covers my hand completely and with a groan he collapses against the tabletop.` },
-      { text:`I let my thumb slip out of him, though I do give his ass one last hard slap with my cum covered hand before I let him get away.` },
-      { text:`He responds with a loud squeak, followed by a chuckle as he reaches behind to rub his sore ass.` },
-      { actorSpeaker:'C', text:`"Mmm, thank you {{P::character.title}}."` },
+      { text:`I slowly start rubbing his cock, keeping my hand gripped tightly around it as he begins to thrust into my
+          hand. My other hand keeps squeezing and kneading his sore ass cheeks, reminding him of the soreness there to
+          keep his recent beating in the forefront of his mind.` },
+      { text:`As I rub his ass my thumb strays closer to his ass crack, parting his cheeks while tugging at the side of
+          his asshole. As his thrusting begins to grow more urgent I relax my grip around his cock, giving him just
+          enough grip to let him hump my hand freely while still giving him just a little bit of friction.` },
+      { text:`My hand presses against his ass in time with his thrusting while my thumb rests fully on his asshole, not
+          entering him, just rubbing and pushing against the rim.` },
+      { text:`He doesn't last much longer, but as soon as I feel him tense up and his cock begins to twitch I thrust my
+          thumb fully into his ass, causing him to cry out in both pain and pleasure.` },
+      { text:`His cum soon covers my hand completely and with a groan he collapses against the tabletop. I let my thumb
+          slip out of him, though I do give his ass one last hard slap with my cum covered hand before I let him get
+          away.` },
+      { actorSpeaker:'C', text:`He responds with a loud squeak, followed by a chuckle as he reaches behind to rub his
+          sore ass. "Mmm, thank you {{P::character.title}}."` },
     ]
   },{
     pages:[
       { text:'...' },
       { text:'..' },
       { text:'.' },
-      { narratorSpeaker:true, text:`Crafting has been unlocked.`, alert:{ unlock:'Crafting Tasks' }},
-      { narratorSpeaker:true, text:`When planning your day's activity you can now add tasks to your plan.` },
-      { narratorSpeaker:true, text:`You can start by crafting a basket to help your minion carry more when out foraging.`, alert:{ unlock:'Craft: Basket' }},
+      { narratorSpeaker:true, text:`Crafting has been unlocked. When planning your day's activity you can now add
+          crafting tasks to your daily plan. You can start by crafting a basket to help your minion carry more when out
+          foraging`,  alert:{ unlock:'Crafting' }},
     ]
   }],
 
--- data/faingorn/events/found-willow-branches.js
+++ data/faingorn/events/found-willow-branches.js
@@ -3,26 +3,22 @@ Event.build('found-willow-branches', {
 
   stages:[{
     pages:[
-      { text:`As instructed, {{C::character.firstName}} returns from today's foraging with several armfuls of willow branches.` },
-      { text:`I take the time to look over what {{C::gender.he}} brought.` },
-      { text:`It's good wood.` },
-      { text:`Green still, but that's perfect for making a basket.` },
-      { text:`Something in the back of my mind is telling me that it's ridiculous for me to have to do this.` },
-      { text:`I feel reduced somehow that I'm planning on dedicating so much time to something as menial as basket weaving.` },
-      { text:`I still don't quite understand.` },
-      { text:`It's something from a previous life blending into this one.` },
-      { text:`It doesn't matter.` },
-      { text:`If I'm to get anywhere in this life I need to work with what I have, and currently that's a rat and a pile of sticks.` },
-      { text:`I pick one up and give it a couple of swishes in the air.` },
-      { text:`<i>Good and slashy,</i> indeed&hellip;` },
+      { text:`As instructed, {{C::character.firstName}} returns from today's foraging with several armfuls of willow
+          branches. I take the time to look over what {{he}} brought. It's good wood. Green still, but that's perfect
+          for making a basket.` },
+      { text:`Something in the back of my mind is telling me that it's ridiculous for me to have to do this. I feel
+          reduced somehow that I'm planning on dedicating so much time to something as menial as basket weaving.` },
+      { text:`I still don't quite understand. It's something from a previous life blending into this one, but it
+          doesn't matter. If I'm to get anywhere in this life I need to work with what I have, and currently that's a
+          rat and a pile of sticks.` },
+      { text:`I pick one up and give it a couple of swishes in the air. <i>Good and slashy,</i> indeed&hellip;` },
       { text:`I look over at my minion who's glancing nervously over at me.` },
     ]
   },
 
   // There are a few different paths that this event can take depending on the player's sexual preferences and the
   // choice they make here. First if the player might have sex with character we set sex=possible.
-  {
-    requires:['player.accepts-women','minion(C).has-pussy'],
+  { requires:['player.accepts-women','minion(C).has-pussy'],
     setChoice:{ sex:'possible' },
     pages:[{ text:`Shall I see how well they work?` }]
   },{
@@ -32,53 +28,41 @@ Event.build('found-willow-branches', {
   },
 
   // If a character would not have sex with their minion we skip right to the end.
-  {
-    requires:['player.never-fucks-women','minion(C).has-pussy'],
+  { requires:['player.never-fucks-women','minion(C).has-pussy'],
     setChoice:{ end:'yes' },
-    pages:[
-      { text:`No&hellip;` },
-      { text:`Perhaps if she had a lovely ballsack that was in need of a good whipping.` },
-      { text:`A propper whipping is far to sexual after all.` },
-    ]
+    pages:[{ text:`No&hellip; Perhaps if she had a lovely ballsack that was in need of a good whipping. A propper
+      whipping is far to sexual after all.` }]
   },{
     requires:['player.never-fucks-men','minion(C).has-cock'],
     setChoice:{ end:'yes' },
-    pages:[
-      { text:`No&hellip;` },
-      { text:`Perhaps if he had some lovely tits that were in need of a good whipping.` },
-      { text:`A propper whipping is far to sexual after all.` },
-    ]
+    pages:[{ text:`No&hellip; Perhaps if he had some lovely tits that were in need of a good whipping. A propper
+      whipping is far to sexual after all.` }]
   },
 
   // If the player might have sex with this minion, we ask if they would like to whip them.
-  {
-    choice:{ sex:'possible' },
+  { choice:{ sex:'possible' },
     selectionPage: true,
     selectionKey: 'whip',
     selections:[
-      { text:`No, I need to keep {{C::gender.him}} healthy for now.`,           value:'no',  effects:['player sadist -1']},
-      { text:`Yes, perhaps it will motivate {{C::gender.him}} to work harder.`, value:'yes', effects:['player sadist 1']},
+      { text:`No, I need to keep {{him}} healthy for now.`,           value:'no',  effects:['player sadist -1']},
+      { text:`Yes, perhaps it will motivate {{him}} to work harder.`, value:'yes', effects:['player sadist 1']},
     ]
   },
 
   // If they don't want to whip the character than this stage is played, which also then plays the following end stage.
   // If they did choose to whip their minion then a follow on event is queued and these last two stages are skipped.
-  {
-    choice:{ whip:'no' },
+  { choice:{ whip:'no' },
     setChoice:{ end:'yes' },
-    pages:[
-      { text:`No.` },
-      { text:`It's an intriguing thought, but whipping {{C::gender.him}} as much as I fear I would like to, might result in {{C::gender.him}} not being able to walk the next day, and that would impact our food supply.` },
-      { text:`One day for sure, just not today.` },
-    ]
+    pages:[{ text:`No. It's an intriguing thought, but if I were to whip {{him}} as much as I would like to {{he}}
+      might not be able to walk the next day, and that would impact our food supply. Perhaps one day, but not today.` }]
   },{
     choice:{ end:'yes' },
     pages:[
-      { text:`I put the stick back on the pile and thank {{C::character.firstName}} for {{C::gender.his}} hard work today.` },
-      { text:`{{C::gender.He}} nods and bows and slinks out of the room looking relieved.` },
-      { narratorSpeaker:true, text:`Crafting has been unlocked.`, alert:{ unlock:'Crafting Tasks' }},
-      { narratorSpeaker:true, text:`When planning your day's activity you can now add tasks to your plan.` },
-      { narratorSpeaker:true, text:`You can start by crafting a basket to help your minion carry more when out foraging.`, alert:{ unlock:'Craft: Basket' }},
+      { text:`I put the stick back on the pile and thank {{C::character.firstName}} for {{his}} hard work today. {{He}}
+          nods and bows and slinks out of the room looking relieved.` },
+      { narratorSpeaker:true, text:`Crafting has been unlocked. When planning your day's activity you can now add
+          crafting tasks to your daily plan. You can start by crafting a basket to help your minion carry more when out
+          foraging`,  alert:{ unlock:'Crafting' }},
     ]
   }],
 
