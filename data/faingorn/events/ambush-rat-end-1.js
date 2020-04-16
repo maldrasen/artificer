ed9fa49ca05c56d6736285839dd60443cc3b398c
Maldrasen <maldrasen@gmail.com>
2020-04-16 12:29:42 -0400

    Update end 1

--- data/faingorn/events/ambush-rat-end-1.js
+++ data/faingorn/events/ambush-rat-end-1.js
@@ -4,17 +4,15 @@ Event.build('ambush-rat-end-1', {
 
   stages:[{
     pages:[
-      { text:`The day has just about passed.` },
-      { text:`There's little time to do anything else, and I'm just exhausted regardless.` },
-      { text:`I stoke the fire in the great hall's fireplace and arrange my pile of sleeping furs close by.` },
-      { text:`My mind drifts, thinking back on the events of today.` },
-      { text:`That scaven, {{R::gender.he}} was rather sexy&hellip; for a rat at least.` },
-      { text:`Absolutely filthy of course, and no doubt riddled with all manner of disease, but lythe and muscular too.` },
-      { text:`Given a bath and some serious disinfectants, I could see sharing a bed with {{R::gender.him}}.` },
-      { text:`With the events of the last couple of days, I haven't been able to give much thought to sex or even my own sexuality yet.` },
-      { text:`I wonder, what sort of partners do I prefer?` },
-      { text:`Men? Women? Animals? Corpses?` },
-      { text:`I should take my time, experiment for a bit, although when I think of my ideal partners they would be&hellip;` },
+      { text:`The day has just about passed. There's little time to do anything else, and I'm just exhausted
+              regardless. I stoke the fire in the great hall's fireplace and arrange my pile of sleeping furs as my
+              mind drifts, thinking back on the events of today.` },
+      { text:`That scaven, {{he}} was rather sexy&hellip; for a rat at least. Absolutely filthy of course, and no doubt
+              riddled with all manner of disease, but lythe and muscular too. Given a bath and some serious
+              disinfectants, I could see sharing a bed with {{him}}.` },
+      { text:`Come to think of it, despite the overly sexual nature of this keep, I really haven't thought much about
+              my own sexuality yet. I wonder, what sort of partners do I prefer? Men? Women? Animals? Corpses?` },
+      { text:`I should take my time, experiment for a bit, although when I think of my ideal partners&hellip;` },
     ]
   },{
     formPage:'sexuality-form',
