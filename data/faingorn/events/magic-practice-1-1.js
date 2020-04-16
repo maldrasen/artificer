9952902f5919e6e249218d9a3eb4a8b2c39ca6a5
Maldrasen <maldrasen@gmail.com>
2020-04-16 18:22:50 -0400

    Magic 1

--- data/faingorn/events/magic-practice-1-1.js
+++ data/faingorn/events/magic-practice-1-1.js
@@ -3,26 +3,23 @@ Event.build('magic-practice-1-1', {
 
   stages:[{
     pages:[
-      { text:`I find a comfortable place to sit on the pile of hides and fur that has been serving as my bed.` },
-      { text:`I close my eyes, emptying my mind of all thought and just sit and listen for a while.` },
-      { text:`I'm not trying to do anything at the moment, just listening. Just breathing.` },
-      { text:`In time, I begin to feel centered; calm, and in control of my sensations.` },
-      { text:`It's not that I don't hear the howling of the wind outside, but it's isolated now, the sensation of hearing is put away for the moment.` },
-      { text:`I reach out with my other senses, tapping into what I feel when I attempt something magical.` },
-      { text:`I'm still not trying to do anything really.` },
-      { text:`Just reaching out.` },
+      { text:`I first find a comfortable place to sit on the pile of hides and fur that has been serving as my bed. I
+          close my eyes, emptying my mind of all thought and just sit and listen for a while. I'm not trying to do
+          anything at the moment, just listening. Just breathing.` },
+      { text:`In time, I begin to feel centered; calm, and in control of my sensations. It's not that I don't hear the
+          howling of the wind outside, but it's isolated now, the sensation of hearing is put away for the moment. I
+          reach out with my other senses, tapping into what I feel when I attempt something magical.` },
+      { text:`I'm not trying to do anything other than breathe and observe.` },
       { text:`If I can lift a rock with magic, I should be able to <i>feel</i> with it as well.` },
       { text:`It's faint, but it's there.` },
-      { text:`There's a sensation of walls around me.` },
-      { text:`I can <i>feel</i> that they're hard and cool to the touch.` },
-      { text:`I can <i>feel</i> the waning heat of the embers from last night's fire, still smoldering in the fireplace.` },
-      { text:`That's&hellip; about the extend of it though.` },
-      { text:`I'm still weak.` },
-      { text:`It feels like I'm flexing a limb I haven't used in too long.` },
+      { text:`There's a sensation of walls all around me. I can <i>feel</i> that they're hard and cool to the touch and
+          I can <i>feel</i> the waning heat of the embers from last night's fire, still smoldering in the fireplace.` },
+      { text:`That's&hellip; about the extend of it though. I'm still weak. It feels like I'm flexing a limb I haven't
+          used in too long.` },
       { text:`I open my eyes again.` },
       { text:`From the light on the floor I can tell I spent longer on that than it felt like.` },
-      { text:`It was surprisingly tiring as well.` },
-      { text:`Still, I think morning meditations like this will help me to explore my powers and should also help build my stamina for using them.` },
+      { text:`It was surprisingly tiring as well. Still, I think morning meditations like this will help me to explore
+          my powers and should also help build my stamina for using them.` },
     ]
   }],
 
