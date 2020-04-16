201b7313533c8ad476f945c928dbb8ba98e95405
Maldrasen <maldrasen@gmail.com>
2020-04-16 17:06:40 -0400

    More small updates

--- data/faingorn/events/morning-6.js
+++ data/faingorn/events/morning-6.js
@@ -6,12 +6,14 @@ Event.build('morning-6', {
 
   stages:[{
     pages:[
-      { text:`I wake up, well a bit horny if I'm being completely honest; thinking about some of the fun that could be had with a little minion willing to serve me in any way I see fit.` },
-      { text:`I could always summon {{C::gender.him}} to me before I send {{C::gender.him}} out on {{C::gender.his}} daily tasks.` },
-      { narratorSpeaker:true, text:`You can now summon your minions to attend to your needs in the morning.`, alert:{ unlock:'Summon Minion' }},
-      { narratorSpeaker:true, text:`Having sex with your minions can change their sexual tastes over time.` },
-      { narratorSpeaker:true, text:`For now, you should only select actions that your minions consent to, If you want to have a positive effect on their sexual skills.` },
-      { narratorSpeaker:true, text:`They might not always be willing to do what you'd like to do, and repeatedly raping your minions leaves them rather cross with you and lowers their effectiveness at other tasks.` },
+      { text:`I wake up, well a bit horny if I'm being completely honest; thinking about some of the fun that could be
+          had with my little minion willing to serve me in any way I see fit. I could always summon {{him}} to me
+          before I send {{him}} out on {{his}} daily tasks.` },
+      { narratorSpeaker:true, text:`You can now summon your minions to attend to your needs in the morning. Having sex
+          with your minions can change their sexual tastes over time. For now, you should only select actions that your
+          minions consent to, if you want to have a positive effect on their sexual skills.`, alert:{ unlock:'Summon Minion' }},
+      { narratorSpeaker:true, text:`They might not always be willing to do what you'd like to do, but repeatedly raping
+          your minions leaves them rather cross with you and lowers their effectiveness at other tasks.` },
     ]
   }],
 
--- data/faingorn/events/starving-single.js
+++ data/faingorn/events/starving-single.js
@@ -6,9 +6,9 @@ Event.build('starving-single', {
 
   stages:[{
     pages:[
-      { text:`I'm not sure what happened.` },
-      { text:`{{C::character.firstName}} should have been able to forage enough food for the both of us, and yet there isn't enough for the both of us to eat.` },
-      { text:`I need a way to find a way to fix this, and fast.` },
+      { text:`I'm not sure what happened. {{C::character.firstName}} should have been able to forage enough food for
+          the both of us, and yet there isn't enough for the both of us to eat. I need a way to find a way to fix this,
+          and fast.` },
       { text:`&hellip;hunger can drive one to do terrible and desperate things.` },
     ],
   }],
