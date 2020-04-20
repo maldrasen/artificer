8174636a4003f2ad46514f178a062d87b6b07d81
Maldrasen <maldrasen@gmail.com>
2020-04-19 21:31:58 -0400

    Get valid location events

--- engine/models/available-event.js
+++ engine/models/available-event.js
@@ -1,6 +1,7 @@
 global.AvailableEvent = Database.instance().define('available_event', {
   code:          { type:Sequelize.STRING },
-  eventType:     { type:Sequelize.STRING },
+  eventType:     { type:Sequelize.STRING, validate:{ isIn:[['location','normal']] }},
+  location:      { type:Sequelize.STRING },
   state_json:    { type:Sequelize.STRING },
   requires_json: { type:Sequelize.STRING },
   chance:        { type:Sequelize.INTEGER },
@@ -16,15 +17,20 @@ global.AvailableEvent = Database.instance().define('available_event', {
   }
 });
 
-// Data should have the following format:
+// Data can have the following attributes, but they're all optional. The
+// requirements and the chance can both be defined on the event itself, but
+// they can be set when enqueued if they're based on some other current value.
 //   state:        {}
 //   requires:     []
 //   chance:       1-100 percent chance of happening
 AvailableEvent.add = async function(code, data={}) {
-  let event = Event.lookup(code);
+  const setting = Event.lookup(code).setting;
+  const type = (Game.EventPhases[setting.phase].type == 'control') ? 'location' : 'normal';
 
   return await AvailableEvent.create({
     code:          code,
+    eventType:     type,
+    location:      setting.location,
     state_json:    JSON.stringify(data.state||{}),
     requires_json: JSON.stringify(data.requires||[]),
     chance:        data.chance || 100,
@@ -36,15 +42,40 @@ AvailableEvent.printAll = async function() {
 
   console.log("\n=== Printing Available Events ===");
   each(events, async event => {
-    let valid = await CentralScrutinizer.meetsRequirements(event.requires)
-    console.log(`    ${event.code}  -  Requires:${event.requires} Valid:${valid}`);
+    console.log(`    ${event.code} - Valid:${await event.isValid()}`);
   });
 }
 
+// Get all of the valid location events for this location.
 AvailableEvent.locationEventsAt = async function(location) {
-
+  return await validateEvents((await AvailableEvent.findAll({ where:{ type:'location', location:validLocations() }})));
 }
 
+// Get all valid location events.
 AvailableEvent.locationEvents = async function() {
+  return await validateEvents((await AvailableEvent.findAll({ where:{ type:'location' }})));
+}
 
+AvailableEvent.prototype.isValid = async function() {
+  return (await CentralScrutinizer.meetsRequirements(this.requires)) &&
+         (await CentralScrutinizer.meetsRequirements(Event.lookup(this.code).requires))
+}
+
+async function validateEvents(events) {
+  return ArrayUtility.compact(await Promise.all(events.map(async event => {
+    return (await event.isValid()) ? event : null;
+  })));
+}
+
+// Sometimes a location may be represented in several ways in the event. If the
+// event setting is set to void it's valid for any location.
+function validLocations() {
+  const current = Game.instance().location;
+  const locations = [current,'void'];
+
+  if (current == Flag.lookup('player.bed-location')) {
+    locations.push('player-bedroom');
+  }
+
+  return locations;
 }
