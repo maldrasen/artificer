c42b757a020ff645c61ca6eaf0a4bc95c2887768
Maldrasen <maldrasen@gmail.com>
2020-04-29 11:20:35 -0400

    Update event text

--- data/faingorn/events/rat-thief-strikes-again.js
+++ data/faingorn/events/rat-thief-strikes-again.js
@@ -2,18 +2,20 @@ Event.build('rat-thief-strikes-again', {
   repeatable: true,
 
   setting: {
-    phase: 'early',
+    phase: 'wake',
     location: 'great-hall',
   },
 
-  requires:[
-    'resource.blood-berries>=24',
-  ],
+  requires:['resource.blood-berries>=24'],
 
   stages:[{
     pages:[
-      { text:`(*) It looks like the rat thief has struck again.`},
-      { text:`(*) I need to finish that storeroom in order to catch him.`},
+      { text:`I wake just as the light of dawn streams in through the great halls windows. I stand and stretch and then
+          curse out loud when I glance over at my supply pile.` },
+      { playerSpeaker:true, text:`"Dammit" It would seem that our scaven thief has struck again. The pile of blood
+          berries that I had intended to use as bait are gone. He's a bold fucker too, steeling them from the very room
+          I was sleeping in.` },
+      { text:`I need to build that storeroom and use it to catch him in the act.` },
     ]
   }],
 
