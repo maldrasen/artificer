4b1275a48057e55ac64c0edbb43bba401f020443
Maldrasen <maldrasen@gmail.com>
2020-05-22 22:06:14 -0400

    Calculate count and tier

--- engine/roles/hunter/results.js
+++ engine/roles/hunter/results.js
@@ -1,17 +1,52 @@
 Role.Hunter.Results = (function() {
 
   // Calculate the results of the hunt.
-  //
-  // options: { character, skill, injured }
-  //
-  // TODO: This function will also consider the hunting location at some point
-  //       so that minions hunting in the Caverns, Greenwood, or Mephidross
-  //       will bring back different shit.
-  //
+  //   options: { character, skill, injured }
 
   async function hunt(options) {
-    const huntMap = buildFrequencyMap(options)
-    return []
+    const huntCount = await calculateCount(options);
+    const huntMap = await buildFrequencyMap(options);
+
+    console.log("Count",huntCount);
+    console.log("Map",huntMap);
+
+    return [];
+  }
+
+  // Count is determined by looking at the character's health, skill, and if
+  // they were injured while hunting.
+  async function calculateCount(options) {
+    let health = await options.character.getHealthClass();
+    let skill = `s${options.skill}`;
+
+    // Health > 75 (healthy or injured)
+    let lowerRange = { s0:0, s1:0, s2:1, s3:2 }[skill];
+    let upperRange = { s0:4, s1:6, s2:8, s3:10 }[skill];
+
+    // Health 51-75
+    if (health == 'bad') {
+      lowerRange = 0;
+      upperRange = { s0:2, s1:4, s2:6, s3:8 }[skill];
+    }
+
+    // Health 26-50
+    if (health == 'horrible') {
+      lowerRange = 0;
+      upperRange = { s0:1, s1:2, s2:4, s3:6 }[skill];
+    }
+
+    // Health 1-25
+    if (health == 'critical') {
+      lowerRange = 0;
+      upperRange = { s0:1, s1:1, s2:2, s3:3 }[skill];
+    }
+
+    // Injured while hunting today. Half the possible count.
+    if (options.injured) {
+      upperRange = Math.ceil(upperRange/2);
+    }
+
+    return Random.between(lowerRange,upperRange);
   }
 
   // The hunting frequency map determines what kind of animals you're likely to
@@ -19,52 +54,40 @@ Role.Hunter.Results = (function() {
   // are you equipped with? If you're hunting bare handed you'll have a harder
   // time than someone who's armed with a sling, or even with teeth and claws.
   // Skill level is also used to determine what's even possible for you to
-  // catch, with some animals being restricted to higher tiers.
+  // catch, with some animals being restricted to higher tiers. Where are you
+  //
+  // TODO: Currently, there are only two possible states. With sling, or
+  //       without sling. Eventually I'll expand it to a much more in depth
+  //       system, but that isn't needed yet.
+  //
+  // TODO: This function will also consider the hunting location at some point
+  //       so that minions hunting in the Caverns, Greenwood, or Mephidross
+  //       will bring back different shit.
+  //
   async function buildFrequencyMap(options) {
-    console.log("Build Frequency Map")
+    let weapon = await options.character.getEquipment('weapon');
 
-    const weapon = await character.getEquipment('weapon');
-    if (weapon == null) { return 0; }
-    if (weapon.code == 'sling') { return 1; }
+    if (weapon == null) {
+      return { tier:'teeth', map:{
+        'rabbit-pelt':   2,
+        'other-meat':    5,
+        'squirrel-meat': 7,
+        'rat-meat':      10,
+      }};
+    }
 
+    if (weapon.code == 'sling') {
+      return { tier:'sling', map:{
+        'fox-pelt':      3,
+        'raccoon-pelt':  5,
+        'rat-meat':      7,
+        'rabbit-pelt':   10,
+        'squirrel-meat': 15,
+      }};
+    }
 
+    throw "Minion is armed, but can they hunt with that weapon?"
   }
-
-  // Hunting tier is based entirely on a character's equipment, primarily on
-  // the weapon used, though other tools will play into this later I think.
-  // Poison tipped arrows are thing, but is ammo a tool then?
-  //   Tier 0 - No equipment.
-  //   Tier 1 - Sling.
-  //   Tier 2 - Bow and arrows.
-  //   Tier 3 - Bow and arrows with poison.
-  async function calculateTier(character) {
-    const weapon = await character.getEquipment('weapon');
-    if (weapon == null) { return 0; }
-    if (weapon.code == 'sling') { return 1; }
-  }
-
-
-  // function huntingResults(tier, skill) {
-  //   if (tier == 0) {
-  //     return {
-  //       skill_0:[{ type:'small-game', min:0, max:1 },{ type:'small-game-pelt', min:0, max:1 }],
-  //       skill_1:[{ type:'small-game', min:1, max:2 },{ type:'small-game-pelt', min:0, max:1 }],
-  //       skill_2:[{ type:'small-game', min:2, max:4 },{ type:'small-game-pelt', min:1, max:2 }],
-  //       skill_3:[{ type:'small-game', min:3, max:6 },{ type:'small-game-pelt', min:1, max:2 }],
-  //     }[`skill_${skill}`];
-  //   }
-  //
-  //   if (tier == 1) {
-  //     return {
-  //       skill_0:[{ type:'small-game', min:3, max:6  },{ type:'small-game-pelt', min:0, max:2 }],
-  //       skill_1:[{ type:'small-game', min:4, max:8  },{ type:'small-game-pelt', min:0, max:3 }],
-  //       skill_2:[{ type:'small-game', min:5, max:10 },{ type:'small-game-pelt', min:1, max:4 }],
-  //       skill_3:[{ type:'small-game', min:6, max:12 },{ type:'small-game-pelt', min:3, max:5 }],
-  //     }[`skill_${skill}`];
-  //   }
-  //
-  //   throw `Need to implement results for hunting tier ${tier}`;
-  // }
 
   // // Get an ItemFlavor of the item type. The flavors we get from hunting are
   // // converted into items when they go into the inventory. rat-meat becomes a
