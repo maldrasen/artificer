67a4c2f9648034b17bac656140b8b43b2a592f79
Maldrasen <maldrasen@gmail.com>
2020-06-04 17:04:40 -0400

    Adding descriptions to courses

--- data/artificer/courses/bonding.js
+++ data/artificer/courses/bonding.js
@@ -1,4 +1,9 @@
 Course.build('bonding', {
   name: 'Bonding',
   category: 'social',
+
+  description: `{{C::character.firstName}} and I will spend some time together, just chatting about things and getting
+    to know each other better. {{He}}'ll grow to like and trust me more if we spend time together which will make
+    {{him}} more loyal to me in the long run.`,
+
 });
--- data/artificer/courses/fucking.js
+++ data/artificer/courses/fucking.js
@@ -3,6 +3,8 @@ Course.build('fucking', {
   category: 'sexual',
   requires: 'sex.fuckingPossible',
 
+  description: normalDescription,
+
   sexAction: new SexAction({
     difficulty: 3,
     effects: 'ass-pussy',
@@ -25,9 +27,43 @@ Course.build('fucking', {
     conflicting: ['anal-averse'],
 
     styles:{
-      rough:{ difficulty:4, complementing:['submissive'], conflicting:['dominant']},
-      abusive:{ difficulty:5, complementing:['masochist'],  conflicting:['dominant']},
+      rough:{ difficulty:4, complementing:['submissive'], conflicting:['dominant'], description:roughDescription },
+      abusive:{ difficulty:5, complementing:['masochist'],  conflicting:['dominant'], description:abusiveDescription },
     }
   }),
 
 });
+
+
+function position(context) {
+  return context.player.cock ?
+    (context.get('C').cock ? 'giving-ass' : 'giving-ass/pussy'):
+    (context.get('C').cock ? 'taking-ass/pussy' : 'dildos');
+}
+
+async function normalDescription(context) {
+  return {
+    'giving-ass':       `I'm going to fuck {{C::character.firstName}} in the ass.`,
+    'giving-ass/pussy': `I'm going to fuck {{C::character.firstName}} in {{his}} ass and pussy.`,
+    'taking-ass/pussy': `{{C::character.firstName}} is going to fuck me.`,
+    'dildos':           `I'm going to fuck {{C::character.firstName's}} ass and pussy with something phallic.`,
+  }[position(context)];
+}
+
+async function roughDescription(context) {
+  return {
+    'giving-ass':       `I'm going to slap {{C::character.firstName}} around while I fuck {{him}} in the ass as hard as I like.`,
+    'giving-ass/pussy': `I'm going to slap {{C::character.firstName}} around while I fuck {{him}} hard in {{his}} ass and pussy.`,
+    'taking-ass/pussy': `I'm going to straddle {{C::character.firstName}} and slap {{him}} around while I'm fucking {{him}}.`,
+    'dildos':           `I'm going to stuff {{C::character.firstName's}} ass and pussy with with something big and phallic.`,
+  }[position(context)];
+}
+
+async function abusiveDescription(context) {
+  return {
+    'giving-ass':       `I'm going to slap, choak, and abusivly fuck the shit out of {{C::character.firstName}}.`,
+    'giving-ass/pussy': `I'm going to slap, choak, and abusivly fuck the shit out of {{C::character.firstName}}.`,
+    'taking-ass/pussy': `I'm going to straddle {{C::character.firstName}} and harshly abuse {{him}} while I'm riding {{his}} cock.`,
+    'dildos':           `I'll brutally pound {{C::character.firstName's}} fuck holes with something huge and phallic.`,
+  }[position(context)];
+}
