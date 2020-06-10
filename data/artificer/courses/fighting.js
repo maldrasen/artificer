c52f0ac346b64694d1c16f92b55d64439db389b6
Maldrasen <maldrasen@gmail.com>
2020-06-10 13:59:58 -0400

    Add story placeholders

--- data/artificer/courses/fighting.js
+++ data/artificer/courses/fighting.js
@@ -73,13 +73,97 @@ async function execute(plan) {
   }
 
   if (skills.player <= 0) {
-    if (skills.situation == 'same') {  }
-    if (skills.situation == 'stronger-minion') { }
-    if (skills.situation == 'much-stronger-minion') { }
+    if (skills.situation == 'same') { writeFumblingThrough(result); }
+    if (skills.situation == 'stronger-minion') { writeShownBasics(result); }
+    if (skills.situation == 'much-stronger-minion') { writeAssKicked(result); }
+  }
+  if (skills.player == 1) {
+    if (skills.situation == 'stronger-player') { writeDecentTraining(result); }
+    if (skills.situation == 'same') { writeEvenMatch(result); }
+    if (skills.situation == 'stronger-minion') { writeGetTrained(result); }
+  }
+  if (skills.player == 2) {
+    if (skills.situation == 'stronger-player') { writeTrainer(result); }
+    if (skills.situation == 'same') { writeSparring(result); }
+    if (skills.situation == 'stronger-minion') { writeLearnFromMaster(result);}
+  }
+  if (skills.player == 3) {
+    if (skills.situation == 'much-stronger-player') { writeKickAss(result); }
+    if (skills.situation == 'stronger-player') { writeMasterTrainer(result); }
+    if (skills.situation == 'same') { writeMasterDuel(result); }
   }
 
-  result.story = 'Did a fighting'
   return result;
+}
+
+// ==============
+// Story Segments
+// ==============
+// Not sure under what conditions all of these variations might become
+// possible. I'm going to start out slowly for now, only implementing the
+// stories that can actually happen. I haven't done it now, but I think I may
+// need to limit the skill level that can be gained by just training with your
+// minions. The upper tiers may need to be unlocked somehow. Something to
+// figure out later though.
+
+// You and your minion are not good fighters and fumble through your training.
+function writeFumblingThrough(result) {
+  result.story = 'TODO: Fumbling Through'
+}
+
+// You're not a good fighter, but your minion knows a thing or two.
+function writeShownBasics(result) {
+  result.story = `TODO: {{C::character.firstName}} shows me some basic techniques.`
+}
+
+// You're not a good fighter, and your minion kicks your ass.
+function writeAssKicked(result) {
+  result.story = `TODO: {{C::character.firstName}} completely kicks my ass.`
+}
+
+// You're an ok fighter, but your minion is worse. Show them a thing or two.
+function writeDecentTraining(result) {
+  result.story = `TODO: I'm able to teach {{C::character.firstName}} a few useful techniques.`
+}
+
+// You and your minion are just ok fighters and have an even match.
+function writeEvenMatch(result) {
+  result.story = `TODO: {{C::character.firstName}} and I are evenly matched and spar for a while.`
+}
+
+// You're an ok fighter, but your minion is better and trains you a bit.
+function writeGetTrained(result) {
+  result.story = `TODO: {{C::character.firstName}} teaches me some more advanced techniques.`
+}
+
+// You're a good fighter, and teach your minion how to fight.
+function writeTrainer(result) {
+  result.story = `TODO: I teach {{C::character.firstName}} a few more advanced techniques.`
+}
+
+// Both you and your minion are good fighters and spar well together.
+function writeSparring(result) {
+  result.story = `TODO: {{C::character.firstName}} and I are both good fighters and spar for a while.`
+}
+
+// You're a good fighter, but your minion is a true master.
+function writeLearnFromMaster(result) {
+  result.story = `TODO: {{C::character.firstName}} is a marshal arts master and trains me to become even better.`
+}
+
+// You're a marshal arts master, and kick your minion's ass.
+function writeKickAss(result) {
+  result.story = `TODO: I completely kick {{C::character.firstName's}} ass.`
+}
+
+// You're a marshal arts master and train your minion.
+function writeMasterTrainer(result) {
+  result.story = `TODO: I am a marshal arts master and I train {{C::character.firstName}} to become even better.`
+}
+
+// You and your minion are both marshal arts masters.
+function writeMasterDuel(result) {
+  result.story = `TODO: {{C::character.firstName}} and I are both marshal arts masters and have an epic duel.`
 }
 
 // The improveSkill() function will get training level from 1 to 4 which is
