70ecb9012bc7149aa6857e207a485426d5aaf070
Maldrasen Des'Tempre <maldrasen@gmail.com>
2020-09-30 13:20:17 -0400

    Yarn is fucking this up

--- bin/spec.sh
+++ bin/spec.sh
@@ -10,8 +10,8 @@ for package in "${packages[@]}"
 do
   if [ $1 = "--coverage" ]
   then
-    yarn run nyc --reporter=html mocha packages/$package/spec/**/*-spec.js --name $package --verbose
+    ./node_modules/.bin/nyc --reporter=html mocha packages/$package/spec/**/*-spec.js --name $package --verbose
   else
-    yarn run mocha packages/$package/spec/**/*-spec.js --name $package $1
+    ./node_modules/.bin/mocha packages/$package/spec/**/*-spec.js --name $package $1
   fi
 done
