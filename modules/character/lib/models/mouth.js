4a9916f822df09a4698193fd10cee4af8d83ba2e
Maldrasen Des'Tempre <maldrasen@gmail.com>
2020-10-20 12:54:29 -0400

    Rewrite mouth

--- modules/character/lib/models/mouth.js
+++ modules/character/lib/models/mouth.js
@@ -1,22 +1,26 @@
-global.Mouth = Database.instance().define('mouth', {
-  parent_id:         { type:Sequelize.INTEGER },
-  parent_class:      { type:Sequelize.STRING },
-  throatWidth:       { type:Sequelize.INTEGER },
-  tongueLength:      { type:Sequelize.INTEGER },
-  tongueShape:       { type:Sequelize.STRING, validate:{ isIn:[['normal','forked','cock']] }},
-  width:             { type:Sequelize.INTEGER },
-  smashLevel:        { type:Sequelize.INTEGER, validate:{ min:0, max:5 }},
-  smashCount:        { type:Sequelize.INTEGER },
-  smashHealing:      { type:Sequelize.INTEGER },
-  smashTeethMissing: { type:Sequelize.INTEGER },
-  cutLevel:          { type:Sequelize.INTEGER, validate:{ min:0, max:5 }},
-  cutCount:          { type:Sequelize.INTEGER },
-  cutHealing:        { type:Sequelize.INTEGER },
-},{
-  timestamps: false,
-  getterMethods: {
-    convertedTongueLength() { return ConversionUtility.milliToInches(this.tongueLength); },
-    comfortableThroatWidth() { return Math.ceil(this.throatWidth * 0.8); },
-    comfortableWidth() { return Math.ceil(this.width * 0.8); },
+global.Mouth = class Mouth extends Model {
+
+  static async createModel() {
+    await this.buildProxy('mouth',{
+      parent_id:         { type:Sequelize.INTEGER },
+      parent_class:      { type:Sequelize.STRING },
+      throatWidth:       { type:Sequelize.INTEGER },
+      tongueLength:      { type:Sequelize.INTEGER },
+      tongueShape:       { type:Sequelize.STRING, validate:{ isIn:[['normal','forked','cock']] }},
+      width:             { type:Sequelize.INTEGER },
+      smashLevel:        { type:Sequelize.INTEGER, validate:{ min:0, max:5 }},
+      smashCount:        { type:Sequelize.INTEGER },
+      smashHealing:      { type:Sequelize.INTEGER },
+      smashTeethMissing: { type:Sequelize.INTEGER },
+      cutLevel:          { type:Sequelize.INTEGER, validate:{ min:0, max:5 }},
+      cutCount:          { type:Sequelize.INTEGER },
+      cutHealing:        { type:Sequelize.INTEGER },
+    });
   }
-});
+
+  get convertedTongueLength() { return ConversionUtility.milliToInches(this.tongueLength); }
+  get comfortableThroatWidth() { return Math.ceil(this.throatWidth * 0.8); }
+  get comfortableWidth() { return Math.ceil(this.width * 0.8); }
+}
+
+Database.registerModel(Mouth);
